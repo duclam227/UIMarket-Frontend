7e0736f76e989c4882b4bde47f1bd23e679cedfe
KoahF <khoakg2013@gmail.com>
2022-03-03 16:00:24 +0700

    Set up routing, add some routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './App.css';
+import NavBar from './components/common/navbar';
+import Pricing from './components/pages/pricing';
+import NotFound from './components/pages/notFound';
+import About from './components/pages/about';
+import Contact from './components/pages/contact';
+import Home from './components/pages/home';
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<NavBar></NavBar>
+			<Routes>
+				<Route path="/about" element={<About />}></Route>
+				<Route path="/contact" element={<Contact />}></Route>
+				<Route path="/pricing" element={<Pricing />}></Route>
+				<Route path="/home" element={<Home />}></Route>
+				<Route path="/" element={<Navigate replace to="/home" />}></Route>
+				<Route path="*" element={<NotFound />}></Route>
+			</Routes>
 		</div>
 	);
 }
