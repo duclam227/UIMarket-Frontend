61b4e712d8d52e9a7c76baaef291db017c3574af
duclam227 <duclam227@gmail.com>
2022-03-05 14:44:25 +0700

    put current pages components into their own folder

---
+++ src/pages/ContactUsPage/Contact.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+const Contact = () => {
+	return (
+		<div>
+			<Container>
+				<h1>Contact Us</h1>
+			</Container>
+		</div>
+	);
+};
+
+export default Contact;
---
+++ src/pages/Homepage/Homepage.tsx
@@ -0,0 +1,13 @@
+import Container from 'react-bootstrap/Container';
+
+const Homepage = () => {
+	return (
+		<div>
+			<Container>
+				<h1>Homepage</h1>
+			</Container>
+		</div>
+	);
+};
+
+export default Homepage;
