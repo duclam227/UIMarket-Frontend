f3deaab15e4fbb38640a5b965c78b7e3ca05dfc4
duclam227 <duclam227@gmail.com>
2022-03-05 19:32:56 +0700

    refactor some code

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,4 @@
-export { default as Homepage } from './Homepage/Homepage';
+export { default as QuestionListsPage } from './QuestionsListPage/QuestionListsPage';
 export { default as AboutUsPage } from './AboutUsPage/About';
 export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as PricingPage } from './PricingPage/Pricing';
