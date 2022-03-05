e23afd0de9dc959bde9754cce1c9992a90c81463
duclam227 <duclam227@gmail.com>
2022-03-05 19:31:07 +0700

    adjust Route and style for main App

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 import { Navbar } from './components';
-import { AboutUsPage, ContactUsPage, Homepage, NotFoundPage, PricingPage } from './pages';
+import { AboutUsPage, ContactUsPage, QuestionListsPage, NotFoundPage, PricingPage } from './pages';
 
-function App () {
+function App() {
 	return (
 		<div className='App'>
 			<Navbar />
@@ -13,8 +13,8 @@ function App () {
 				<Route path='/about' element={<AboutUsPage />} />
 				<Route path='/contact' element={<ContactUsPage />} />
 				<Route path='/pricing' element={<PricingPage />} />
-				<Route path='/home' element={<Homepage />} />
-				<Route path='/' element={<Navigate replace to='/home' />} />
+				<Route path='/questions/*' element={<QuestionListsPage />} />
+				<Route path='/' element={<Navigate replace to='/questions' />} />
 				<Route path='*' element={<NotFoundPage />} />
 			</Routes>
 		</div>
