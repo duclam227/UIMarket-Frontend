49ea6524dfec182fed71efbcee77a6cbaea21f96
KoahF <khoakg2013@gmail.com>
2022-03-07 17:03:43 +0700

    Fix missing properties for question interface

--- src/pages/QuestionsListPage/QuestionListsPage.tsx
+++ src/pages/QuestionsListPage/QuestionListsPage.tsx
@@ -8,46 +8,69 @@ import { QuestionList, PageWithNavbar } from '../../components';
 import style from './QuestionListsPage.module.css';
 
 const QuestionListsPage: FC = () => {
+  const tabList = [
+    {
+      path: 'all',
+      label: 'All',
+    },
+    {
+      path: 'bountied',
+      label: 'Bountied',
+    },
+    {
+      path: 'popular',
+      label: 'Popular',
+    },
+  ];
 
-	const tabList = [
-		{
-			path: 'all',
-			label: 'All',
-		},
-		{
-			path: 'bountied',
-			label: 'Bountied',
-		},
-		{
-			path: 'popular',
-			label: 'Popular',
-		},
-	]
+  return (
+    <>
+      <PageWithNavbar>
+        <div className={style.container}>
+          <div className={style.mainContent}>
+            <h1 className={style.title}>
+              <FormattedMessage
+                id="QuestionListsPage.title"
+                defaultMessage="All Questions"
+              />
+            </h1>
+            <QuestionListsPageNavigator tabList={tabList} />
 
-	return (
-		<>
-			<PageWithNavbar>
-				<div className={style.container}>
-					<div className={style.mainContent}>
-						<h1 className={style.title}>
-							<FormattedMessage id="QuestionListsPage.title" defaultMessage="All Questions" />
-						</h1>
-						<QuestionListsPageNavigator tabList={tabList} />
-
-						<div className={style.questionsList}>
-							<Routes>
-								<Route path="all" element={<QuestionList questionsList={[{ question: '1' }, { question: '2' }]} />} />
-								<Route path="bountied" element={<p>bountied hey</p>} />
-								<Route path="popular" element={<p>popular yo</p>} />
-								<Route path="/*" element={<Navigate replace to='all' />} />
-							</Routes>
-						</div>
-					</div>
-				</div>
-			</PageWithNavbar>
-
-		</>
-	);
+            <div className={style.questionsList}>
+              <Routes>
+                <Route
+                  path="all"
+                  element={
+                    <QuestionList
+                      questionsList={[
+                        {
+                          title: '',
+                          body: '',
+                          tags: [],
+                          bounty: 0,
+                          question: '1',
+                        },
+                        {
+                          title: '',
+                          body: '',
+                          tags: [],
+                          bounty: 0,
+                          question: '2',
+                        },
+                      ]}
+                    />
+                  }
+                />
+                <Route path="bountied" element={<p>bountied hey</p>} />
+                <Route path="popular" element={<p>popular yo</p>} />
+                <Route path="/*" element={<Navigate replace to="all" />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </PageWithNavbar>
+    </>
+  );
 };
 
 export default QuestionListsPage;
