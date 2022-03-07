939e24dac47f82d853ba63ce34c119a14872d9c8
KoahF <khoakg2013@gmail.com>
2022-03-07 17:02:28 +0700

    Add properties for question interface

--- src/app/util/interfaces.ts
+++ src/app/util/interfaces.ts
@@ -1,10 +1,14 @@
 export interface question {
-	question: string;
+  title: string;
+  body: string;
+  tags: string[];
+  bounty: number;
+  question: string;
 }
 
 export interface navigatorTabItem {
-	path: string;
-	label: string;
+  path: string;
+  label: string;
 }
 
 export interface authCredentials {
