89b32fddce0ec3105cb743d26f76035e4e1b6505
duclam227 <duclam227@gmail.com>
2022-03-08 22:49:54 +0700

    add email and password fields to login form

--- src/forms/LoginForm/LoginForm.tsx
+++ src/forms/LoginForm/LoginForm.tsx
@@ -1,20 +1,48 @@
 import { FC } from "react";
 import { Button, Form } from "react-bootstrap";
-import { FormattedMessage } from "react-intl";
+import { FormattedMessage, injectIntl } from "react-intl";
 
 import style from './LoginForm.module.css';
 
 const LoginForm: FC = () => {
 
+  //general login form labels
   const title = <FormattedMessage id="LoginForm.title" defaultMessage="Sign in" />
+  const continueWithGoogleLabel = <FormattedMessage
+    id="LoginForm.continueWithGoogleLabel" defaultMessage="Continue with Google" />
+  const continueWithFacebookLabel = <FormattedMessage
+    id="LoginForm.continueWithFacebookLabel" defaultMessage="Continue with Facebook" />
+
+  //login form fields labels
+  const loginFormEmailLabel = <FormattedMessage
+    id="LoginForm.emailLabel" defaultMessage="Email" />
+  const loginFormPasswordLabel = <FormattedMessage
+    id="LoginForm.passwordLabel" defaultMessage="Password" />
 
   return (
     <>
       <div className={style.loginFormContainer}>
         <h2 className={style.title}>{title}</h2>
-        <Button>Continue with Google</Button>
+        <div className={style.otherIdpButtonRow}>
+          <Button>{continueWithGoogleLabel}</Button>
+          <Button>{continueWithFacebookLabel}</Button>
+        </div>
         <hr />
-        <Form></Form>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>{loginFormEmailLabel}</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>{loginFormPasswordLabel}</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
       </div>
     </>
   )
