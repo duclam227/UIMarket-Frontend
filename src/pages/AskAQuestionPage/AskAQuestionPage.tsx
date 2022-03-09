4257749e52da83488b1208518f0446ff8ec00837
KoahF <khoakg2013@gmail.com>
2022-03-09 15:03:18 +0700

    rebase conflict Add react-intl to AskAQuestionPage

--- src/pages/AskAQuestionPage/AskAQuestionPage.tsx
+++ src/pages/AskAQuestionPage/AskAQuestionPage.tsx
@@ -1,6 +1,8 @@
 import { SyntheticEvent, useState } from 'react';
 import classNames from 'classnames';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { FormattedMessage, injectIntl } from 'react-intl';
+
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
@@ -16,7 +18,7 @@ import { PageWithNavbar } from '../../components';
 import './AskAQuestionPage.css';
 import style from './AskAQuestionPage.module.css';
 
-const Ask = () => {
+const Ask = ({ intl }: any) => {
   const formGroupClassName = 'mb-3';
   const cardClassName = 'mb-4';
   const containerClassName = classNames(style.pageContainer, 'w-75');
@@ -24,6 +26,64 @@ const Ask = () => {
   const topUpButtonClassName = 'mb-3';
   const topUpGroupClassName =
     'd-flex flex-column align-items-end justify-content-evenly';
+
+  const pageTitle = (
+    <FormattedMessage
+      id="AskAQuestionPage.pageTitle"
+      defaultMessage="Ask a question"
+    />
+  );
+  const questionTitle = (
+    <FormattedMessage id="AskAQuestionPage.questionTitle" defaultMessage="Title" />
+  );
+  const questionTitlePlaceholder = intl.formatMessage({
+    id: 'AskAQuestionPage.questionTitlePlaceholder',
+    defaultMessage: 'e.g How do I...',
+  });
+  const questionBody = (
+    <FormattedMessage id="AskAQuestionPage.questionBody" defaultMessage="Body" />
+  );
+  const questionTags = (
+    <FormattedMessage id="AskAQuestionPage.questionTags" defaultMessage="Tags" />
+  );
+  const questionTagsPlaceholder = intl.formatMessage({
+    id: 'AskAQuestionPage.questionTagsPlaceholder',
+    defaultMessage: 'e.g UI, color, alignment, ...',
+  });
+  const addBountyLabel = (
+    <FormattedMessage
+      id="AskAQuestionPage.addBountyLabel"
+      defaultMessage="Add a bounty"
+    />
+  );
+  const addBountyDescription = (
+    <FormattedMessage
+      id="AskAQuestionPage.addBountyDescription"
+      defaultMessage="Bountied questions will appear highlighted and can attract more answers. You need credit to add bounty to a question"
+    />
+  );
+  const addBountyBalanceLabel = (
+    <FormattedMessage
+      id="AskAQuestionPage.addBountyBalanceLabel"
+      defaultMessage="Balance: $"
+    />
+  );
+  const addBountyTopUpBtnText = (
+    <FormattedMessage
+      id="AskAQuestionPage.addBountyTopUpBtnText"
+      defaultMessage="Top up"
+    />
+  );
+  const addBountyInputPlaceholder = intl.formatMessage({
+    id: 'AskAQuestionPage.addBountyInputPlaceholder',
+    defaultMessage: '$10',
+  });
+  const submitBtnText = (
+    <FormattedMessage
+      id="AskAQuestionPage.submitBtnText"
+      defaultMessage="Post Question"
+    />
+  );
 
   const [question, setQuestion] = useState<question>({
     title: '',
@@ -38,29 +98,31 @@ const Ask = () => {
   const handleChange = ({ currentTarget: input }: any) => {
     setQuestion({ ...question, [input.id]: input.value });
   };
-
+  const handleSubmit = () => {
+    console.log(question);
+  };
   return (
     <PageWithNavbar>
       <Container className={containerClassName}>
-        <h1 className={style.pageTitle}>Ask a question</h1>
+        <h1 className={style.pageTitle}>{pageTitle}</h1>
 
         <Card className={cardClassName}>
           <Card.Body>
             <Form>
               <Form.Group className={formGroupClassName} controlId="title">
                 <Form.Label>
-                  <h4>Title</h4>
+                  <h4>{questionTitle}</h4>
                 </Form.Label>
                 <Form.Control
+                  placeholder={questionTitlePlaceholder}
                   type="text"
-                  placeholder="Title"
                   onChange={(e) => handleChange(e)}
                 />
               </Form.Group>
 
               <Form.Group className={formGroupClassName}>
                 <Form.Label htmlFor="body">
-                  <h4>Body</h4>
+                  <h4>{questionBody}</h4>
                 </Form.Label>
                 <CKEditor
                   editor={ClassicEditor}
@@ -73,11 +135,11 @@ const Ask = () => {
 
               <Form.Group className={formGroupClassName}>
                 <Form.Label>
-                  <h4>Tags</h4>
+                  <h4>{questionTags}</h4>
                 </Form.Label>
                 <Form.Control
                   type="text"
-                  placeholder="e.g UI, color, alignment, ..."
+                  placeholder={questionTagsPlaceholder}
                 />
               </Form.Group>
             </Form>
@@ -88,16 +150,16 @@ const Ask = () => {
           <Card.Body>
             <Row>
               <Col md={8}>
-                <h4>Add a bounty</h4>
-                <p className="text-muted">
-                  Bountied questions will appear highlighted and can attract
-                  more answers. You need credit to add bounty to a question
-                </p>
+                <h4>{addBountyLabel}</h4>
+                <p className="text-muted">{addBountyDescription}</p>
               </Col>
               <Col md={4} className={topUpGroupClassName}>
-                <h4>Balance: ${balance}</h4>
+                <h4>
+                  {addBountyBalanceLabel}
+                  {balance}
+                </h4>
                 <Button variant="warning" className={topUpButtonClassName}>
-                  Top up
+                  {addBountyTopUpBtnText}
                 </Button>
               </Col>
             </Row>
@@ -106,7 +168,7 @@ const Ask = () => {
               <Form.Group controlId="bounty">
                 <Form.Control
                   type="number"
-                  placeholder="$10"
+                  placeholder={addBountyInputPlaceholder}
                   onChange={(e) => handleChange(e)}
                 />
               </Form.Group>
@@ -114,12 +176,16 @@ const Ask = () => {
           </Card.Body>
         </Card>
 
-        <Button variant="primary" className={postQuestionButtonClassName}>
-          Post Question
+        <Button
+          variant="primary"
+          className={postQuestionButtonClassName}
+          onClick={handleSubmit}
+        >
+          {submitBtnText}
         </Button>
       </Container>
     </PageWithNavbar>
   );
 };
 
-export default Ask;
+export default injectIntl(Ask);
