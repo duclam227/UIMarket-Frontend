27552adc97b5a830e69330122c3228fd6f6c8257
KoahF <khoakg2013@gmail.com>
2022-03-09 15:02:07 +0700

    Move export to match alphabetical order

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,9 +1,9 @@
 /*===Alphabetically arranged===*/
 
 export { default as AboutUsPage } from './AboutUsPage/About';
+export { default as AskAQuestionPage } from './AskAQuestionPage/AskAQuestionPage';
 export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as LoginPage } from './LoginPage/LoginPage';
 export { default as NotFoundPage } from './NotFoundPage/NotFound';
 export { default as PricingPage } from './PricingPage/Pricing';
 export { default as QuestionListsPage } from './QuestionsListPage/QuestionListsPage';
-export { default as AskAQuestionPage } from './AskAQuestionPage/AskAQuestionPage';
