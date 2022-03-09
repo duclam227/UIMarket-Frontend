9679c38af74f9dda7d9c4829f7a0cf1f20d4ee27
duclam227 <duclam227@gmail.com>
2022-03-09 20:54:22 +0700

    add messages to login form: forgot password, sign up

--- src/forms/LoginForm/LoginForm.tsx
+++ src/forms/LoginForm/LoginForm.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Button, Form } from "react-bootstrap";
 import { FormattedMessage, injectIntl, IntlShape } from "react-intl";
+import { Link } from "react-router-dom";
 
 import style from './LoginForm.module.css';
 
@@ -13,6 +14,14 @@ const LoginForm: FC<loginFormProps> = (props) => {
 
   //general login form labels
   const title = <FormattedMessage id="LoginForm.title" defaultMessage="Sign in" />
+  const forgotPasswordMessage = <FormattedMessage
+    id="LoginForm.forgotPassword" defaultMessage="Forgot your password?" />
+  const newToUsMessage = <FormattedMessage
+    id="LoginForm.newToUs" defaultMessage="New to us?" />;
+  const signUpMessage = <FormattedMessage
+    id="LoginForm.signUp" defaultMessage="Sign up." />
+
+  //button labels
   const submitMessage = <FormattedMessage
     id="LoginForm.submit" defaultMessage="Sign in" />
   const continueWithGoogleLabel = <FormattedMessage
@@ -46,20 +55,28 @@ const LoginForm: FC<loginFormProps> = (props) => {
         </div>
         <hr />
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>{loginFormEmailLabel}</Form.Label>
+          <Form.Group className="mb-3">
+            <Form.Label className={style.label}>{loginFormEmailLabel}</Form.Label>
             <Form.Control type="email" placeholder={loginFormEmailPlaceholder} />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>{loginFormPasswordLabel}</Form.Label>
+          <Form.Group className="mb-3">
+            <Form.Label className={style.label}>{loginFormPasswordLabel}</Form.Label>
             <Form.Control type="password" placeholder={loginFormPasswordPlaceholder} />
+            <Form.Text>
+              <Link className={style.forgetPasswordText} to="/">{forgotPasswordMessage}</Link>
+            </Form.Text>
           </Form.Group>
 
-          <Button variant="primary" type="submit">
+          <Button variant="primary" type="submit" className={style.submitButton}>
             {submitMessage}
           </Button>
         </Form>
+
+        <div className={style.bottomWrapper}>
+          <span>{newToUsMessage}</span>
+          <Link to="/signup">{signUpMessage}</Link>
+        </div>
       </div>
     </>
   )
