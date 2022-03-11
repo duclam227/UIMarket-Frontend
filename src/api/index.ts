59e5ad595828a6e37db38552c21a45ded15fdeb1
KoahF <khoakg2013@gmail.com>
2022-03-11 16:12:58 +0700

    add authService, attach jwt to all request

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,26 +1,29 @@
 import axios from 'axios';
+import { getJwt } from '../app/util/authHelpers';
 
+const jwt = getJwt();
 const axiosClient = axios.create({
-	baseURL: process.env.REACT_APP_BASE_SERVER_URL,
-	headers: {
-		'content-type': 'application/json',
-	},
+  baseURL: process.env.REACT_APP_BASE_SERVER_URL,
+  headers: {
+    'content-type': 'application/json',
+    Authentication: `Bearer ${jwt}`,
+  },
 });
 
 axiosClient.interceptors.request.use(async config => {
-	return config;
+  return config;
 });
 
 axiosClient.interceptors.response.use(
-	response => {
-		if (response && response.data) {
-			return response.data;
-		}
-		return response;
-	},
-	error => {
-		throw error;
-	},
+  response => {
+    if (response && response.data) {
+      return response.data;
+    }
+    return response;
+  },
+  error => {
+    throw error;
+  },
 );
 
 export default axiosClient;
