1da98e6ed4e4cc0560bc1548d7f41519db21bc7a
duclam227 <duclam227@gmail.com>
2022-03-11 20:44:45 +0700

    add route for SignUp

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 
 import './App.css';
 import { Navbar } from './components';
-import { AboutUsPage, ContactUsPage, QuestionListsPage, NotFoundPage, PricingPage, LoginPage } from './pages';
+import { AboutUsPage, ContactUsPage, QuestionListsPage, NotFoundPage, PricingPage, AuthenticationPage } from './pages';
 
 function App() {
 	return (
 		<div className='App'>
 			{/* <Navbar /> */}
 			<Routes>
-				<Route path='/login' element={<LoginPage />} />
+				<Route path='/login' element={<AuthenticationPage destination='login' />} />
+				<Route path='/signup' element={<AuthenticationPage destination='signup' />} />
 				<Route path='/about' element={<AboutUsPage />} />
 				<Route path='/contact' element={<ContactUsPage />} />
 				<Route path='/pricing' element={<PricingPage />} />
