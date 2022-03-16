18e6c6bc6a3777f9bebe06c3b39d788c24796191
duclam227 <duclam227@gmail.com>
2022-03-16 13:52:06 +0700

    initialize Paginator folder

---
+++ src/components/common/Paginator/Paginator.tsx
@@ -0,0 +1,32 @@
+import { FC } from 'react';
+import { Pagination } from 'react-bootstrap';
+
+import { PaginatorProps } from '../../../app/util/interfaces';
+
+import style from 'Paginator.module.css';
+
+const Paginator: FC<PaginatorProps> = (props) => {
+  const { totalNumberOfPages, currentPage } = props;
+
+  return (
+    <Pagination>
+      <Pagination.First />
+      <Pagination.Prev />
+      <Pagination.Item>{1}</Pagination.Item>
+      <Pagination.Ellipsis />
+
+      <Pagination.Item>{10}</Pagination.Item>
+      <Pagination.Item>{11}</Pagination.Item>
+      <Pagination.Item active>{12}</Pagination.Item>
+      <Pagination.Item>{13}</Pagination.Item>
+      <Pagination.Item disabled>{14}</Pagination.Item>
+
+      <Pagination.Ellipsis />
+      <Pagination.Item>{20}</Pagination.Item>
+      <Pagination.Next />
+      <Pagination.Last />
+    </Pagination>
+  )
+}
+
+export default Paginator;
