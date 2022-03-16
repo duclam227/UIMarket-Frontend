9b78e4ec3614a73cd6351a7f0d3ec3d8c96db78d
duclam227 <duclam227@gmail.com>
2022-03-16 13:51:51 +0700

    initialize ViewQuestionpage folder

---
+++ src/pages/ViewQuestionPage/ViewQuestionPage.tsx
@@ -0,0 +1,15 @@
+import { PageWithNavbar } from "../../components";
+
+import style from './ViewQuestionPage.module.css';
+
+const ViewQuestionPage = () => {
+  return (
+    <PageWithNavbar>
+      <div className={style.container}>
+
+      </div>
+    </PageWithNavbar>
+  )
+}
+
+export default ViewQuestionPage;
