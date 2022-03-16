4c9595066331b5af1732ea8eea23ead41e85bd17
duclam227 <duclam227@gmail.com>
2022-03-16 14:32:38 +0700

    get id of question from url

--- src/pages/ViewQuestionPage/ViewQuestionPage.tsx
+++ src/pages/ViewQuestionPage/ViewQuestionPage.tsx
@@ -1,12 +1,17 @@
+import { useParams, useSearchParams } from "react-router-dom";
+
 import { PageWithNavbar } from "../../components";
 
 import style from './ViewQuestionPage.module.css';
 
 const ViewQuestionPage = () => {
+  const { id } = useParams();
+  const questionId = id || '';
+
   return (
     <PageWithNavbar>
       <div className={style.container}>
-
+        {questionId}
       </div>
     </PageWithNavbar>
   )
