5280e59d1560267cdc6d6dba0c98e8553edabfd7
duclam227 <duclam227@gmail.com>
2022-03-16 16:53:55 +0700

    finish logic and functions for paginator

--- src/components/common/Paginator/Paginator.tsx
+++ src/components/common/Paginator/Paginator.tsx
@@ -1,3 +1,4 @@
+import { render } from '@testing-library/react';
 import { FC } from 'react';
 import { Pagination } from 'react-bootstrap';
 
@@ -5,27 +6,120 @@ import { PaginatorProps } from '../../../app/util/interfaces';
 
 import style from './Paginator.module.css';
 
+const SMALL_LIMIT = 5;
+const BIG_LIMIT = 10;
+
 const Paginator: FC<PaginatorProps> = (props) => {
   const { totalNumberOfPages, currentPage } = props;
+
+  if (totalNumberOfPages === 1) {
+    return null;
+  }
+
+  const renderButtons = () => {
+    if (currentPage < SMALL_LIMIT) {
+      if (totalNumberOfPages >= BIG_LIMIT) {
+        //render 1 -> 10
+        const pagingButtons = [];
+        for (let i = 1; i <= 10; i++) {
+          pagingButtons.push(
+            <Pagination.Item
+              active={currentPage === i ? true : false}
+              onClick={(number) => goToPage(i)}
+            >{i}</Pagination.Item>
+          )
+        }
+        return pagingButtons;
+      } else {
+        //render 1 -> total
+        const pagingButtons = [];
+        for (let i = 1; i <= totalNumberOfPages; i++) {
+          pagingButtons.push(
+            <Pagination.Item
+              active={currentPage === i ? true : false}
+              onClick={(number) => goToPage(i)}
+            >{i}</Pagination.Item>
+          )
+        }
+        return pagingButtons;
+      }
+    } else {
+      if (totalNumberOfPages >= BIG_LIMIT) {
+        //render 1 -> 10
+        const firstNumber = currentPage - SMALL_LIMIT;
+        const lastNumber = currentPage + SMALL_LIMIT - 1;
+        const pagingButtons = [];
+        for (let i = firstNumber; i <= lastNumber; i++) {
+          pagingButtons.push(
+            <Pagination.Item
+              active={currentPage === i ? true : false}
+              onClick={(number) => goToPage(i)}
+            >{i}</Pagination.Item>
+          )
+        }
+        return pagingButtons;
+      } else {
+        //render 1 -> total
+        const pagingButtons = [];
+        for (let i = 1; i <= totalNumberOfPages; i++) {
+          pagingButtons.push(
+            <Pagination.Item
+              active={currentPage === i ? true : false}
+              onClick={(number) => goToPage(i)}
+            >{i}</Pagination.Item>
+          )
+        }
+        return pagingButtons;
+      }
+    }
+  }
+
+  const goToPage = (pageNumber: number) => {
+    if (pageNumber === currentPage) {
+      return;
+    }
+
+    props.handleClickGoToPage(pageNumber);
+  }
+
+  const nextPage = () => {
+    if (currentPage === totalNumberOfPages) {
+      return;
+    }
+
+    props.handleClickGoToPage(currentPage + 1);
+  }
+
+  const previousPage = () => {
+    if (currentPage === 1) {
+      return;
+    }
+
+    props.handleClickGoToPage(currentPage - 1);
+  }
+
+  const firstpage = () => {
+    props.handleClickGoToPage(1);
+  }
 
   return (
     <div className={style.container}>
       <Pagination className={style.paginator}>
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
+        {currentPage === 1
+          ? null
+          : <>
+            <Pagination.First onClick={() => props.handleClickGoToPage(1)} />
+            <Pagination.Prev onClick={previousPage} />
+          </>}
 
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-        <Pagination.Item disabled>{14}</Pagination.Item>
+        {renderButtons()}
 
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
+        {currentPage === totalNumberOfPages
+          ? null
+          : <>
+            <Pagination.Next onClick={nextPage} />
+            <Pagination.Last onClick={() => props.handleClickGoToPage(totalNumberOfPages)} />
+          </>}
       </Pagination>
     </div>
   )
