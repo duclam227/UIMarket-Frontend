5d852ef4560243c1c0ab0c8e2f59b3366e73dbd8
duclam227 <duclam227@gmail.com>
2022-03-16 13:52:31 +0700

    add Paginator to list of export in components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as QuestionList } from './QuestionList/QuestionList';
 /* =============== Common =============== */
 export { default as Navbar } from './common/Navbar/Navbar';
 export { default as PageWithNavbar } from './common/PageWithNavbar/PageWithNavbar';
+export { default as Paginator } from './common/Paginator/Paginator';
