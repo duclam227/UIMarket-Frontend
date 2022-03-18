5b6fd711f67a9a613d7528df055bf0366291534e
duclam227 <duclam227@gmail.com>
2022-03-18 23:22:32 +0700

    resolve conflict in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,8 @@ import {
   AuthenticationPage,
   AskAQuestionPage,
   ViewQuestionPage,
+  BountiedQuestionListsPage,
+  PopularQuestionListsPage
 } from './pages';
 import { logInWithJWT } from './redux/index';
 
@@ -57,7 +59,10 @@ function App() {
             </RequireUser>
           }
         />
-        <Route path="/questions/*" element={<QuestionListsPage />} />
+        <Route path='/questions/all' element={<QuestionListsPage />} />
+        <Route path='/questions/bountied' element={<BountiedQuestionListsPage />} />
+        <Route path='/questions/popular' element={<PopularQuestionListsPage />} />
+        <Route path='/questions/*' element={<Navigate replace to='/questions/all' />} />
         <Route path="/question/:id" element={<ViewQuestionPage />} />
         <Route path="/" element={<Navigate replace to="/questions" />} />
         <Route path="*" element={<NotFoundPage />} />
