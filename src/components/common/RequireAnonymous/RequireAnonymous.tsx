49bdace5ba3bed6e3f85786fce1a7d23c1147cbb
KoahF <khoakg2013@gmail.com>
2022-03-18 22:16:09 +0700

    Remove unuse hook

--- src/components/common/RequireAnonymous/RequireAnonymous.tsx
+++ src/components/common/RequireAnonymous/RequireAnonymous.tsx
@@ -1,4 +1,4 @@
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { State } from '../../../redux/store';
 
