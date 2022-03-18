76384db8bc84f779303224ad4eba815b28f765d7
duclam227 <duclam227@gmail.com>
2022-03-18 23:19:32 +0700

    export ViewQuestionPage in pages index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,8 +2,9 @@
 
 export { default as AboutUsPage } from './AboutUsPage/About';
 export { default as AskAQuestionPage } from './AskAQuestionPage/AskAQuestionPage';
-export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as AuthenticationPage } from './AuthenticationPage/AuthenticationPage';
+export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as NotFoundPage } from './NotFoundPage/NotFound';
 export { default as PricingPage } from './PricingPage/Pricing';
 export { default as QuestionListsPage } from './QuestionsListPage/QuestionListsPage';
+export { default as ViewQuestionPage } from './ViewQuestionPage/ViewQuestionPage';
