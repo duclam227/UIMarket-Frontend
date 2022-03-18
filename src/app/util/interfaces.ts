8657dc019c3432083a5ba15ef837e4404232c9a2
KoahF <khoakg2013@gmail.com>
2022-03-18 14:51:44 +0700

    Add customer interface, edit auth redux

--- src/app/util/interfaces.ts
+++ src/app/util/interfaces.ts
@@ -12,7 +12,25 @@ export interface navigatorTabItem {
 }
 
 export interface authCredentials {
-	customerEmail: string;
-	customerPassword: string;
-	customerName?: string;
+  customerEmail: string;
+  customerPassword: string;
+  customerName?: string;
+}
+
+export interface customer {
+  customerName: string;
+  customerEmail: string;
+  customerPassword: string;
+  customerPhone: string;
+  customerDOB: string;
+  authenToken: {
+    Google: string;
+  };
+  customerWallet: {
+    coin: string;
+    point: string;
+  };
+  customer_Status: string;
+  createdAt: Date;
+  updatedAt: Date;
 }
--- src/redux/auth/authInterfaces.ts
+++ src/redux/auth/authInterfaces.ts
@@ -1,56 +1,56 @@
 import { actionTypes } from './authActionTypes';
-
+import { customer } from '../../app/util/interfaces';
 interface authLoginRequest {
-	type: actionTypes.AUTH_LOGIN_REQUEST;
-	user?: Object | null;
-	error: string | null;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_LOGIN_REQUEST;
+  user?: Object | null;
+  error: string | null;
+  isInProgress: boolean;
 }
 
 interface authLoginSuccess {
-	type: actionTypes.AUTH_LOGIN_SUCCESS;
-	user: Object;
-	error: null;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_LOGIN_SUCCESS;
+  user: Object;
+  error: null;
+  isInProgress: boolean;
 }
 
 interface authSignUpRequest {
-	type: actionTypes.AUTH_SIGNUP_REQUEST;
-	user?: Object | null;
-	error: string | null;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_SIGNUP_REQUEST;
+  user?: Object | null;
+  error: string | null;
+  isInProgress: boolean;
 }
 
 interface authSignUpSuccess {
-	type: actionTypes.AUTH_SIGNUP_SUCCESS;
-	user: Object;
-	error: null;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_SIGNUP_SUCCESS;
+  user: Object;
+  error: null;
+  isInProgress: boolean;
 }
 
 interface authError {
-	type: actionTypes.AUTH_ERROR;
-	user?: null;
-	error: string;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_ERROR;
+  user?: null;
+  error: string;
+  isInProgress: boolean;
 }
 
 interface authSignOut {
-	type: actionTypes.AUTH_USER_SIGN_OUT;
-	user: null;
-	error: null;
-	isInProgress: boolean;
+  type: actionTypes.AUTH_USER_SIGN_OUT;
+  user: null;
+  error: null;
+  isInProgress: boolean;
 }
 
 export type Action =
-	| authError
-	| authLoginRequest
-	| authLoginSuccess
-	| authSignOut
-	| authSignUpRequest
-	| authSignUpSuccess;
+  | authError
+  | authLoginRequest
+  | authLoginSuccess
+  | authSignOut
+  | authSignUpRequest
+  | authSignUpSuccess;
 export interface authState {
-	user: Object | null;
-	error: string | null;
-	isInProgress: boolean | null;
+  user: customer | null;
+  error: string | null;
+  isInProgress: boolean | null;
 }
