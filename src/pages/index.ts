784a5bf2ece45a89d00f9edac4ea998503fda168
duclam227 <duclam227@gmail.com>
2022-03-18 23:20:40 +0700

    resolve conflict in component index

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -3,8 +3,12 @@
 export { default as AboutUsPage } from './AboutUsPage/About';
 export { default as AskAQuestionPage } from './AskAQuestionPage/AskAQuestionPage';
 export { default as AuthenticationPage } from './AuthenticationPage/AuthenticationPage';
+export {
+	default as BountiedQuestionListsPage,
+} from './QuestionsListPage/BountiedQuestionListsPage';
 export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as NotFoundPage } from './NotFoundPage/NotFound';
+export { default as PopularQuestionListsPage } from './QuestionsListPage/PopularQuestionListsPage';
 export { default as PricingPage } from './PricingPage/Pricing';
 export { default as QuestionListsPage } from './QuestionsListPage/QuestionListsPage';
 export { default as ViewQuestionPage } from './ViewQuestionPage/ViewQuestionPage';
