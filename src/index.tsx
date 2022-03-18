d152e310e7069a768b12bc78acd005bb777f47a9
KoahF <khoakg2013@gmail.com>
2022-03-18 14:49:00 +0700

    Add bootstrap js bundle

--- src/index.tsx
+++ src/index.tsx
@@ -10,19 +10,19 @@ import store from './redux/store';
 
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 ReactDOM.render(
-	<React.StrictMode>
-		<LanguageWrapper>
-			<Provider store={store}>
-				<BrowserRouter>
-					<App />
-				</BrowserRouter>
-			</Provider>
-		</LanguageWrapper>
-	</React.StrictMode>,
-	document.getElementById('root'),
+  <React.StrictMode>
+    <LanguageWrapper>
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    </LanguageWrapper>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
