05ccc9fa1f953f82542bae258229c7ffc330a328
duclam227 <duclam227@gmail.com>
2022-03-19 19:39:37 +0700

    add SectionAnswers to display answer list

---
+++ src/pages/ViewQuestionPage/SectionAnswers.tsx
@@ -0,0 +1,54 @@
+import { FC, useState } from "react";
+import { FormattedMessage } from "react-intl";
+import JsxParser from "react-jsx-parser";
+import { customer } from "../../app/util/interfaces";
+import { Paginator } from "../../components";
+
+import style from './SectionAnswers.module.css';
+import SectionAnswerVoter from "./SectionAnswerVoter";
+
+interface SectionAnswersProps {
+  answerList: Array<any>;
+  currentUser: customer | null;
+  question: any;
+}
+
+const SectionAnswers: FC<SectionAnswersProps> = (props) => {
+  const { answerList, currentUser, question } = props;
+
+  if (!answerList.length) {
+    return null;
+  }
+
+  const renderAnswers = () => {
+    return answerList.map(a => {
+      return (
+        <div className={style.answer}>
+          <div className={style.sideContent}>avt</div>
+          <div className={style.content}>
+            <div className={style.authorInfo}>
+              {a.customerInfo[0].customerName}
+            </div>
+            <JsxParser jsx={a.answerContent} />
+            <SectionAnswerVoter
+              numberOfDownvotes={a.totalDownvote}
+              numberOfUpvotes={a.totalUpvote}
+              question={question}
+              voteStatus={{ upvote: false, downvote: true }}
+              currentUser={currentUser}
+              handleVoteStatus={() => { }}
+            />
+          </div>
+        </div>
+      )
+    })
+  }
+
+  return (
+    <div className={style.answerContainer}>
+      {renderAnswers()}
+    </div>
+  )
+}
+
+export default SectionAnswers;
