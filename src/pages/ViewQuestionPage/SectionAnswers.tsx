ec7762a26b25d73643bf96ef73d6475c3153033b
duclam227 <duclam227@gmail.com>
2022-03-23 21:22:38 +0700

    add voter and reply button to answers

--- src/pages/ViewQuestionPage/SectionAnswers.tsx
+++ src/pages/ViewQuestionPage/SectionAnswers.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from "react";
 import { FormattedMessage } from "react-intl";
 import JsxParser from "react-jsx-parser";
-import { customer } from "../../app/util/interfaces";
+import { customer, voteStatus } from "../../app/util/interfaces";
 import { Paginator } from "../../components";
 
 import style from './SectionAnswers.module.css';
@@ -30,13 +30,18 @@ const SectionAnswers: FC<SectionAnswersProps> = (props) => {
               {a.customerInfo[0].customerName}
             </div>
             <JsxParser jsx={a.answerContent} />
-            <SectionAnswerVoter
-              answer={a}
-              question={question}
-              voteStatus={a.voteStatus}
-              currentUser={currentUser}
-              handleVoteStatus={() => { }}
-            />
+            <div className={style.footerContent}>
+              <SectionAnswerVoter
+                answer={a}
+                question={question}
+                currentUser={currentUser}
+                handleVoteStatus={() => { }}
+              />
+              {a.customerInfo[0].customerEmail === currentUser?.customerEmail
+                ? <div>Reply</div>
+                : null
+              }
+            </div>
           </div>
         </div>
       )
