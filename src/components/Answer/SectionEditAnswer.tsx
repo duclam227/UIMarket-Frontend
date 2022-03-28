5d529f70f4d7290e12f29135ea48f67df126d3b0
duclam227 <duclam227@gmail.com>
2022-03-28 21:32:33 +0700

    add delete, edit answer

---
+++ src/components/Answer/SectionEditAnswer.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import RichTextEditor from "../common/RichTextEditor/RichTextEditor";
+
+import style from './Answer.module.css';
+
+interface Props {
+  initialValue: string;
+  onHide: Function;
+  onSave: Function;
+}
+
+const SectionEditAnswer: React.FC<Props> = (props) => {
+  const { initialValue } = props;
+  const [content, setContent] = useState<string>(initialValue || '');
+
+  const handleSaveAnswer = () => {
+    props.onSave(content);
+    props.onHide();
+  }
+
+  const handleChangeContent = (text: string) => {
+    setContent(text);
+  }
+
+  return (
+    <Form className={style.sectionEdit}>
+      <RichTextEditor initialValue={initialValue} onChange={(text: string) => handleChangeContent(text)} />
+      <Button onClick={() => props.onHide()}>Cancel</Button>
+      <Button onClick={handleSaveAnswer}>Save</Button>
+    </Form>
+  )
+}
+
+export default SectionEditAnswer;
