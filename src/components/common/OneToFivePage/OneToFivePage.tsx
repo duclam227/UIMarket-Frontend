a90eb3c5feafc6445c2b74515371f12890365bbd
KoahF <khoakg2013@gmail.com>
2022-04-19 20:35:52 +0700

    Add render sidenav only when logged in

--- src/components/common/OneToFivePage/OneToFivePage.tsx
+++ src/components/common/OneToFivePage/OneToFivePage.tsx
@@ -1,45 +1,55 @@
-import { Children, FC } from 'react';
-import { Navbar as CustomNavbar } from '../..';
-import SideNav from '../SideNav/SideNav';
+import { FC } from 'react';
+import { useSelector } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
 
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Navbar from 'react-bootstrap/Navbar';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 
+import { State } from '../../../redux/store';
+import { Navbar as CustomNavbar } from '../..';
+import SideNav from '../SideNav/SideNav';
 import style from './OneToFivePage.module.css';
 
 const PageWithSideNav: FC = props => {
+  const currentUser = useSelector((state: State) => state.auth.user);
   const { children } = props;
   return (
     <>
       <div className={style.pageWrapper}>
         <CustomNavbar />
-        <div className={`${style.pageContainer} container-fluid p-0`}>
-          <Row>
-            <Col lg={3} xl={2} className={`p-0 position-sticky top-0`}>
-              <Navbar collapseOnSelect expand="lg" sticky="top">
-                <Navbar.Toggle
-                  aria-controls="offcanvasNavbar"
-                  className={`${style.hamburgerOffcanvasToggler}`}
-                />
-                <Navbar.Offcanvas
-                  id="offcanvasNavbar"
-                  aria-labelledby="offcanvasNavbarLabel"
-                  placement="start"
-                >
-                  <Offcanvas.Body className={`p-0`}>
-                    <SideNav />
-                  </Offcanvas.Body>
-                </Navbar.Offcanvas>
-                <SideNav className={`d-none d-lg-block`} />
-              </Navbar>
-            </Col>
-            <Col lg={9} xl={10} className={`p-2`}>
-              {children}
-            </Col>
-          </Row>
-        </div>
+        {currentUser ? (
+          //Render side nav
+          <div className={`${style.pageContainer} container-fluid p-0`}>
+            <Row>
+              <Col lg={3} xl={2} className={`p-0 position-sticky top-0`}>
+                <Navbar collapseOnSelect expand="lg" sticky="top">
+                  <Navbar.Toggle
+                    aria-controls="offcanvasNavbar"
+                    className={`${style.hamburgerOffcanvasToggler}`}
+                  />
+                  <Navbar.Offcanvas
+                    id="offcanvasNavbar"
+                    aria-labelledby="offcanvasNavbarLabel"
+                    placement="start"
+                  >
+                    <Offcanvas.Body className={`p-0`}>
+                      <SideNav />
+                    </Offcanvas.Body>
+                  </Navbar.Offcanvas>
+                  <SideNav className={`d-none d-lg-block`} />
+                </Navbar>
+              </Col>
+              <Col lg={9} xl={10} className={`p-2`}>
+                {children}
+              </Col>
+            </Row>
+          </div>
+        ) : (
+          //Don't render side nav
+          <div className={style.pageContainer}>{children}</div>
+        )}
       </div>
     </>
   );
