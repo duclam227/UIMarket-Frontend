9d172a672254bb1bb10d94c18cef14e0a7b7aaba
KoahF <khoakg2013@gmail.com>
2022-04-19 20:36:43 +0700

    Add intl messages, move some imports around

--- src/components/common/SideNav/SideNav.tsx
+++ src/components/common/SideNav/SideNav.tsx
@@ -1,25 +1,86 @@
 import { FC } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
 import { useSelector } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router-dom';
+
+import { BsGear, BsWallet, BsBag, BsShop } from 'react-icons/bs';
 
 import { State } from '../../../redux/store';
-
 import style from './SideNav.module.css';
-import { BsGear, BsWallet, BsBag, BsShop } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
 
 interface Props {
   className?: string;
 }
 const SideNav: FC<Props> = props => {
+  const myProfileNavGroupLabel = (
+    <FormattedMessage
+      id="SideNav.myProfileNavGroupLabel"
+      defaultMessage="My Profile"
+    />
+  );
+  const editProfileNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.editProfileNavLinkLabel"
+      defaultMessage="Edit Profile"
+    />
+  );
+  const personalInfoNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.personalInfoNavLinkLabel"
+      defaultMessage="Personal Information"
+    />
+  );
+  const paymentNavGroupLabel = (
+    <FormattedMessage
+      id="SideNav.paymentNavGroupLabel"
+      defaultMessage="Payment"
+    />
+  );
+  const deexWalletNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.deexWalletNavLinkLabel"
+      defaultMessage="DeeX Wallet"
+    />
+  );
+  const bankAccountsNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.bankAccountsNavLinkLabel"
+      defaultMessage="Bank Accounts"
+    />
+  );
+  const purchaseReviewsNavGroupLabel = (
+    <FormattedMessage
+      id="SideNav.purchaseReviewsNavGroupLabel"
+      defaultMessage="Purchase & Reviews"
+    />
+  );
+  const myStoreNavGroupLabel = (
+    <FormattedMessage
+      id="SideNav.myStoreNavGroupLabel"
+      defaultMessage="My Store"
+    />
+  );
+  const productsNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.productsNavLinkLabel"
+      defaultMessage="Products"
+    />
+  );
+  const bankDetailsNavLinkLabel = (
+    <FormattedMessage
+      id="SideNav.bankDetailsNavLinkLabel"
+      defaultMessage="Bank Details"
+    />
+  );
+
   const { className } = props;
   const currentUser = useSelector((state: State) => state.auth.user);
+
   return (
     //Sidenav only for logged in users?
     <div className={`${style.sideNavWrapper} ${className}`}>
+      {/* Current user display */}
       <div className={style.currentUserRow}>
-        {/* Current user display */}
         <div className={style.currentUser}>
           <div className={style.profilePictureWrapper}>
             <img
@@ -35,11 +96,13 @@ const SideNav: FC<Props> = props => {
       <ul className={style.navGroup}>
         <div className={style.navGroupName}>
           <BsGear className={style.navGroupNameIcon} />
-          <span className={style.navGroupNameLabel}>My Profile</span>
+          <span className={style.navGroupNameLabel}>
+            {myProfileNavGroupLabel}
+          </span>
         </div>
-        <li className={style.navLinkItem}>Edit Profile</li>
+        <li className={style.navLinkItem}>{editProfileNavLinkLabel}</li>
         <Link to={`/user/${currentUser?._id}`}>
-          <li className={style.navLinkItem}>Personal Information</li>
+          <li className={style.navLinkItem}>{personalInfoNavLinkLabel}</li>
         </Link>
       </ul>
 
@@ -47,17 +110,21 @@ const SideNav: FC<Props> = props => {
       <ul className={style.navGroup}>
         <div className={style.navGroupName}>
           <BsWallet className={style.navGroupNameIcon} />
-          <span className={style.navGroupNameLabel}>Payment</span>
+          <span className={style.navGroupNameLabel}>
+            {paymentNavGroupLabel}
+          </span>
         </div>
-        <li className={style.navLinkItem}>DeeX Wallet</li>
-        <li className={style.navLinkItem}>Bank Accounts</li>
+        <li className={style.navLinkItem}>{deexWalletNavLinkLabel}</li>
+        <li className={style.navLinkItem}>{bankAccountsNavLinkLabel}</li>
       </ul>
 
       {/* Purchase nav group */}
       <ul className={style.navGroup}>
         <div className={style.navGroupName}>
           <BsBag className={style.navGroupNameIcon} />
-          <span className={style.navGroupNameLabel}>Purchase & Reviews</span>
+          <span className={style.navGroupNameLabel}>
+            {purchaseReviewsNavGroupLabel}
+          </span>
         </div>
       </ul>
 
@@ -65,10 +132,12 @@ const SideNav: FC<Props> = props => {
       <ul className={style.navGroup}>
         <div className={style.navGroupName}>
           <BsShop className={style.navGroupNameIcon} />
-          <span className={style.navGroupNameLabel}>My Store</span>
+          <span className={style.navGroupNameLabel}>
+            {myStoreNavGroupLabel}
+          </span>
         </div>
-        <li className={style.navLinkItem}>Products</li>
-        <li className={style.navLinkItem}>Bank Details</li>
+        <li className={style.navLinkItem}>{productsNavLinkLabel}</li>
+        <li className={style.navLinkItem}>{bankDetailsNavLinkLabel}</li>
       </ul>
     </div>
   );
