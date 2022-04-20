e19b500c30a428c976e89acf27d157f20cd99788
Duc-Lam-jh <duclam227@gmail.com>
2022-04-20 20:21:12 +0700

    add form to deal with uploading images of product

---
+++ src/forms/AddAProductPicturesForm/AddAProductPicturesForm.tsx
@@ -0,0 +1,66 @@
+import React, { useEffect, useState } from "react";
+import { IntlShape, injectIntl } from "react-intl";
+import Form from "react-bootstrap/Form";
+
+import s3API from "../../api/amazonS3";
+
+import { ImageInput } from "../../components";
+
+import style from './AddAProductPicturesForm.module.css';
+
+interface Props {
+  updateProductInfo: Function;
+  intl: IntlShape;
+}
+
+const AddAProductPicturesForm: React.FC<Props> = (props) => {
+  const {
+    intl
+  } = props;
+
+  const [images, setImages] = useState<Array<string>>([]);
+
+  const uploadImage = (image: File) => {
+    s3API.getSignedUrl('images')
+      .then((res: any) => {
+        const signedUploadUrl: string = res.url;
+        s3API.uploadToS3Bucket(signedUploadUrl, image)
+          .then((res: any) => {
+            const imageUrl = signedUploadUrl.split('?')[0];
+            setImages([...images, imageUrl]);
+          })
+          .catch(error => {
+            throw error;
+          })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+  const deleteImage = (indexToDelete: number) => {
+    setImages(images.filter((img, index) => index !== indexToDelete));
+  }
+
+  useEffect(() => {
+    props.updateProductInfo({
+      'productPicture': [...images]
+    })
+  }, [images])
+
+  return (
+    <Form
+      name='AddAProductPicturesForm'
+      className={style.form}
+    >
+      <ImageInput
+        images={images}
+        handleUploadImage={(image: File) => uploadImage(image)}
+        handleDeleteImage={(index: number) => deleteImage(index)}
+      />
+
+    </Form>
+  )
+}
+
+export default injectIntl(AddAProductPicturesForm);
--- src/forms/index.ts
+++ src/forms/index.ts
@@ -1,2 +1,8 @@
+export {
+	default as AddAProductDescriptionForm,
+} from './AddAProductDescriptionForm/AddAProductDescriptionForm';
+export {
+	default as AddAProductPicturesForm,
+} from './AddAProductPicturesForm/AddAProductPicturesForm';
 export { default as LoginForm } from './LoginForm/LoginForm';
 export { default as SignupForm } from './SignupForm/SignupForm';
