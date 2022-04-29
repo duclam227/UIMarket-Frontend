3609f4617416ecdd55a0c3188c7ba8d010c14e3f
Duc-Lam-jh <duclam227@gmail.com>
2022-04-29 19:41:40 +0700

    add api to create shop

--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -3,6 +3,8 @@ import post from './post';
 
 const shopAPI = {
 	getAllProductsOfShop: get.getAllProductsOfShop,
+
+	createShop: post.createShop,
 };
 
 export default shopAPI;
--- src/api/shop/post.ts
+++ src/api/shop/post.ts
@@ -5,6 +5,17 @@ import { product } from '../../app/util/interfaces';
 const SHOP_ENDPOINT = '/api/v1/shop';
 
 class Post {
+	createShop = (shopInfo: any) => {
+		const jwt = getJwt();
+		const config = {
+			headers: {
+				Authorization: `Bearer ${jwt}`,
+			},
+		};
+
+		return axiosClient.post(`${SHOP_ENDPOINT}/register`, shopInfo, config);
+	};
+
 	addNewProduct = (product: product | null) => {
 		const jwt = getJwt();
 		const config = {
