c175f81b1b0511236d7b3b551b90273729b8ad16
Duc-Lam-jh <duclam227@gmail.com>
2022-05-06 21:11:50 +0700

    add search form

--- src/forms/index.ts
+++ src/forms/index.ts
@@ -6,4 +6,5 @@ export {
 } from './AddAProductPicturesForm/AddAProductPicturesForm';
 export { default as CreateAShopForm } from './CreateAShopForm/CreateAShopForm';
 export { default as LoginForm } from './LoginForm/LoginForm';
+export { default as SearchForm } from './SearchForm/SearchForm';
 export { default as SignupForm } from './SignupForm/SignupForm';
