5def8a1ad09512f67a903a373e298e932dfead3e
duclam227 <duclam227@gmail.com>
2022-05-07 16:02:16 +0700

    update: image input can handle multiple file

--- src/components/common/ImageInput/ImageInput.tsx
+++ src/components/common/ImageInput/ImageInput.tsx
@@ -13,84 +13,119 @@ interface Props {
   handleDeleteImage: Function;
 }
 
-const ImageInput: React.FC<Props> = (props) => {
+const ImageInput: React.FC<Props> = props => {
   const { multiple } = props;
   const [images, setImages] = useState<Array<any>>(props.images || []);
   const [prevImages, setPrevImages] = useState<Array<any>>(props.images || []);
-  const [previewImages, setPreviewImages] = useState<Array<any>>(props.images || []);
+  const [previewImages, setPreviewImages] = useState<Array<any>>(
+    props.images || [],
+  );
   const [error, setError] = useState<null | string>(null);
 
-  const handleChange = (e: any) => {
+  const readFileAsURL = (inputFile: File) => {
+    const temporaryFileReader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+      temporaryFileReader.onerror = () => {
+        temporaryFileReader.abort();
+        reject(new DOMException('Problem parsing input file.'));
+      };
+
+      temporaryFileReader.onload = () => {
+        resolve(temporaryFileReader.result);
+      };
+      temporaryFileReader.readAsDataURL(inputFile);
+    });
+  };
+
+  const handleChange = async (e: any) => {
     const imagesInInput = [...e.target.files];
-    imagesInInput.forEach(image => {
-      if (!image.type.startsWith("image/")) {
-        setError("File must be a picture!");
+    const imagesAfterUpload: Array<any> = [];
+    const previewImagesAfterUpload: Array<any> = [];
+
+    for (let i = 0; i < imagesInInput.length; i++) {
+      const image = imagesInInput[i];
+      if (!image.type.startsWith('image/')) {
+        setError('File must be a picture!');
       } else {
         setError(null);
       }
 
-      const reader = new FileReader();
-      reader.readAsDataURL(image);
-      reader.onload = () => {
-        props.handleUploadImage(image);
-        setImages([...images, image]);
-        setPreviewImages([...previewImages, reader.result]);
-        setPrevImages([...previewImages, reader.result]);
-      }
-    })
-  }
+      const imageURL = await readFileAsURL(image);
+      imagesAfterUpload.push(image);
+      previewImagesAfterUpload.push(imageURL);
+      props.handleUploadImage(image);
+    }
+
+    setImages([...images, ...imagesAfterUpload]);
+    setPreviewImages([...previewImages, ...previewImagesAfterUpload]);
+    setPrevImages([...previewImages, ...previewImagesAfterUpload]);
+  };
 
   const handleDeleteImage = (e: any, index: number) => {
     const imagesAfterDelete = images.filter((item, i) => i !== index);
-    const previewImagesAfterDelete = prevImages.filter((item, i) => i !== index);
+    const previewImagesAfterDelete = prevImages.filter(
+      (item, i) => i !== index,
+    );
 
     props.handleDeleteImage(index);
     setImages([...imagesAfterDelete]);
     setPreviewImages([...previewImagesAfterDelete]);
 
     e.preventDefault();
-  }
+  };
 
   useEffect(() => {
     setPrevImages([...previewImages]);
-  }, [previewImages])
+  }, [previewImages]);
+
+  console.log(previewImages);
 
   return (
     <>
       <div className={style.dropzone}>
         <label className={style.dropzoneButton}>
           <RiUpload2Line className={style.uploadIcon} />
-          <div className={style.title}><FormattedMessage id='ImageInput.uploadTitle' /></div>
-          <FormattedMessage id='ImageInput.uploadSubtitle' />
-          <input id='image' name='image' type="file"
-            onChange={(e) => handleChange(e)}
-            accept='image/*'
+          <div className={style.title}>
+            <FormattedMessage id="ImageInput.uploadTitle" />
+          </div>
+          <FormattedMessage id="ImageInput.uploadSubtitle" />
+          <input
+            id="image"
+            name="image"
+            type="file"
+            onChange={e => handleChange(e)}
+            accept="image/*"
             multiple={multiple}
           />
         </label>
         {error && <div>{error}</div>}
         <div className={style.images}>
-          {previewImages && previewImages.map((image: any, index: number) => {
-            return (
-              <div id={'image' + index} key={index} className={style.imageContainer}>
-
-                <div className={style.previewImage}>
-                  <img src={image} alt="Preview" />
-                </div>
-
-                <div className={style.removeImageButton}
-                  onClick={(e) => handleDeleteImage(e, index)}
+          {previewImages &&
+            previewImages.map((image: any, index: number) => {
+              return (
+                <div
+                  id={'image' + index}
+                  key={index}
+                  className={style.imageContainer}
                 >
-                  <RiCloseFill className={style.removeImageIcon} />
+                  <div className={style.previewImage}>
+                    <img src={image} alt="Preview" />
+                  </div>
+
+                  <div
+                    className={style.removeImageButton}
+                    onClick={e => handleDeleteImage(e, index)}
+                  >
+                    <RiCloseFill className={style.removeImageIcon} />
+                  </div>
                 </div>
-              </div>
-            )
-          })}
+              );
+            })}
         </div>
       </div>
-
     </>
-  )
-}
+  );
+};
 
 export default ImageInput;
