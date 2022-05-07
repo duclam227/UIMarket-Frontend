42f216f18c3df9166cfc2543bbe52c7ff014d864
duclam227 <duclam227@gmail.com>
2022-05-07 17:55:24 +0700

    reformat pictures form

--- src/forms/AddAProductPicturesForm/AddAProductPicturesForm.tsx
+++ src/forms/AddAProductPicturesForm/AddAProductPicturesForm.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from "react";
-import { IntlShape, injectIntl, FormattedMessage } from "react-intl";
-import Form from "react-bootstrap/Form";
+import React, { useEffect, useState } from 'react';
+import { IntlShape, injectIntl, FormattedMessage } from 'react-intl';
+import Form from 'react-bootstrap/Form';
 
-import s3API from "../../api/amazonS3";
+import s3API from '../../api/amazonS3';
 
-import { ImageInput } from "../../components";
+import { ImageInput } from '../../components';
 
 import style from './AddAProductPicturesForm.module.css';
 
@@ -13,47 +13,46 @@ interface Props {
   intl: IntlShape;
 }
 
-const AddAProductPicturesForm: React.FC<Props> = (props) => {
-  const {
-    intl
-  } = props;
+const AddAProductPicturesForm: React.FC<Props> = props => {
+  const { intl } = props;
 
   const [images, setImages] = useState<Array<string>>([]);
 
   const uploadImage = (image: File) => {
-    s3API.getSignedUrl('images')
+    s3API
+      .getSignedUrl('images')
       .then((res: any) => {
         const signedUploadUrl: string = res.url;
-        s3API.uploadToS3Bucket(signedUploadUrl, image)
+        s3API
+          .uploadToS3Bucket(signedUploadUrl, image)
           .then((res: any) => {
             const imageUrl = signedUploadUrl.split('?')[0];
             setImages([...images, imageUrl]);
           })
           .catch(error => {
             throw error;
-          })
+          });
       })
       .catch(error => {
         console.log(error);
-      })
-  }
+      });
+  };
 
   const deleteImage = (indexToDelete: number) => {
     setImages(images.filter((img, index) => index !== indexToDelete));
-  }
+  };
 
   useEffect(() => {
     props.updateProductInfo({
-      'productPicture': [...images]
-    })
-  }, [images])
+      productPictures: [...images],
+    });
+  }, [images]);
 
   return (
-    <Form
-      name='AddAProductPicturesForm'
-      className={style.form}
-    >
-      <div className={style.formTitle}><FormattedMessage id='AddAProduct.TabImagesTitle' /></div>
+    <Form name="AddAProductPicturesForm" className={style.form}>
+      <div className={style.formTitle}>
+        <FormattedMessage id="AddAProduct.TabImagesTitle" />
+      </div>
 
       <ImageInput
         multiple={true}
@@ -61,9 +60,8 @@ const AddAProductPicturesForm: React.FC<Props> = (props) => {
         handleUploadImage={(image: File) => uploadImage(image)}
         handleDeleteImage={(index: number) => deleteImage(index)}
       />
-
     </Form>
-  )
-}
+  );
+};
 
 export default injectIntl(AddAProductPicturesForm);
