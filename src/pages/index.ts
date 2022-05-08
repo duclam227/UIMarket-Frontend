39f32c8640ff333209fc8c0ae2dd98bc7313ea23
KoahF <khoakg2013@gmail.com>
2022-05-08 10:56:39 +0700

    Export 2 VerifyMesssage page, reorder some exports

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,14 +1,15 @@
 /*===Alphabetically arranged===*/
 
 export { default as AboutUsPage } from './AboutUsPage/About';
+export { default as AccountNotVerifiedPage } from './AccountNotVerifiedPage/AccountNotVerifiedPage';
 export { default as AddAProductPage } from './AddAProductPage/AddAProductPage';
 export { default as AskAQuestionPage } from './AskAQuestionPage/AskAQuestionPage';
 export { default as AuthenticationPage } from './AuthenticationPage/AuthenticationPage';
-export {
-	default as BountiedQuestionListsPage,
-} from './QuestionsListPage/BountiedQuestionListsPage';
+export { default as BountiedQuestionListsPage } from './QuestionsListPage/BountiedQuestionListsPage';
 export { default as ContactUsPage } from './ContactUsPage/Contact';
 export { default as CreateAShopPage } from './CreateAShopPage/CreateAShopPage';
+export { default as EditPersonalInfoPage } from './EditPersonalInfoPage/EditPersonalInfoPage';
+export { default as EditProfilePage } from './EditProfilePage/EditProfilePage';
 export { default as EditQuestionPage } from './EditQuestionPage/EditQuestionPage';
 export { default as ManageProductsPage } from './ManageProductsPage/ManageProductsPage';
 export { default as NotFoundPage } from './NotFoundPage/NotFound';
@@ -18,8 +19,7 @@ export { default as QuestionListsPage } from './QuestionsListPage/QuestionListsP
 export { default as QuestionListsPageByTag } from './QuestionsListPage/QuestionListsByTagPage';
 export { default as SearchQuestionsPage } from './SearchQuestionsPage/SearchQuestionsPage';
 export { default as ShopHomePage } from './ShopHomePage/ShopHomePage';
+export { default as VerifyPromptPage } from './VerifyPromptPage/VerifyPromptPage';
 export { default as ViewProductPage } from './ViewProductPage/ViewProductPage';
 export { default as ViewQuestionPage } from './ViewQuestionPage/ViewQuestionPage';
 export { default as UserProfilePage } from './UserProfilePage/UserProfilePage';
-export { default as EditProfilePage } from './EditProfilePage/EditProfilePage';
-export { default as EditPersonalInfoPage } from './EditPersonalInfoPage/EditPersonalInfoPage';
