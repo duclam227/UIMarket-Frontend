d1410144438deb82d2dd2d828123f7e98925fb27
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-05-10 12:10:28 +0700

    get products from api and minor ui fix

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,53 +1,78 @@
 import { FC } from 'react';
 import { Container, Card, Row, Col } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
+import { product } from '../../app/util/interfaces';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
+import style from './ProductList.module.css';
 interface Props {
-  //productList: Array<any>;
+  productList: Array<product> | null;
 }
 
 const ProductList: FC<Props> = props => {
-  //const { productList } = props;
+  const { productList } = props;
 
-  //if (!productList || productList.length < 1) {
-  // return null;
-  //}
+  if (!productList || productList.length < 1) {
+    return null;
+  }
+
+  const renderStars = (productRating: number) => {
+    const starArray: Array<any> = [];
+    let rating = productRating;
+    for (let key = 1; key <= 5; key++) {
+      if (rating > 0) {
+        starArray.push(
+          <i className="bi-star-fill text-warning m-1" key={key} />,
+        );
+      } else {
+        starArray.push(
+          <i className="bi-star-fill text-secondary m-1" key={key} />,
+        );
+      }
+      rating = rating - 1;
+    }
+
+    return <div>{starArray}</div>;
+  };
 
   const renderProduct = () => {
-    //return productList.map((p: any, i: number) => {
-    //const pLink = '/product/' + p._id;
-    return (
-      <Col xs={12} md={6} lg={3}>
-        <Card>
-          <Card.Img
-            variant="top"
-            src="https://picsum.photos/200"
-            className=""
-          />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text className="text-secondary">sold</Card.Text>
-            <Card.Text as="h4">Price</Card.Text>
-          </Card.Body>
-        </Card>
-      </Col>
-    );
-    //});
+    return productList.map((p: any, i: number) => {
+      const pLink = '/products/' + p._id;
+      return (
+        <Col key={p._id} xs={12} md={6} lg={3}>
+          <Card className="h-100 border-0 bg-transparent p-0">
+            <div className="shadow-sm">
+              <Card.Img
+                variant="top"
+                src={p.coverPicture}
+                className={style.cardImg}
+                style={{ aspectRatio: '2 / 1' }}
+              />
+            </div>
+            <Card.Body className="px-0 pb-2">
+              <Card.Title>
+                <h6>{p.productName}</h6>
+              </Card.Title>
+              <Card.Text className="text-secondary">
+                by {p.shopId.shopName}
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer className="border-0 bg-transparent p-0">
+              <div className="d-flex justify-content-between ">
+                {renderStars(p.productRating)}
+                <strong>${p.productPrice}</strong>
+              </div>
+            </Card.Footer>
+          </Card>
+        </Col>
+      );
+    });
   };
 
   const renderProductList = () => {
     return (
-      <Container className="py-5">
-        <Row className="justify-content-center gy-4">
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-          {renderProduct()}
-        </Row>
+      <Container fluid="lg" className="py-5">
+        <Row className="justify-content-center gy-5">{renderProduct()}</Row>
       </Container>
     );
   };
