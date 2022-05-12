d629af77e609a6b616f1f068173ab10d7257fea3
KoahF <khoakg2013@gmail.com>
2022-05-12 19:22:13 +0700

    Extract Product component

--- src/pages/ManageProductsPage/ManageProductsPage.tsx
+++ src/pages/ManageProductsPage/ManageProductsPage.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, ChangeEvent } from 'react';
 import { useSelector } from 'react-redux';
+import _ from 'lodash';
 
 import Button from 'react-bootstrap/Button';
 import Spinner from 'react-bootstrap/Spinner';
@@ -14,7 +15,7 @@ import { State } from '../../redux/store';
 import { product } from '../../app/util/interfaces';
 import noProductsImage from '../../app/assets/error-not-found.png';
 
-import { BsPlusCircle, BsGearFill, BsCaretDownFill } from 'react-icons/bs';
+import { BsPlusCircle } from 'react-icons/bs';
 
 import shopAPI from '../../api/shop';
 
@@ -22,6 +23,13 @@ import { OneToFivePage } from '../../components';
 import Product from './Product';
 
 import style from './ManageProductsPage.module.css';
+import productAPI from '../../api/product';
+import { getErrorMessage } from '../../app/util';
+
+interface ProductListItem {
+  isSelected: boolean;
+  product: product;
+}
 
 const ManageProductsPage: React.FC = () => {
   const oneToOnePlaceholderImg =
@@ -30,16 +38,20 @@ const ManageProductsPage: React.FC = () => {
   const currentUser = useSelector((state: State) => state.auth.user);
   const shopId = currentUser?.shopId;
 
-  const [products, setProducts] = useState<Array<product> | null>(null);
+  const [products, setProducts] = useState<ProductListItem[] | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-
   useEffect(() => {
     shopId &&
       shopAPI
         .getAllProductsOfShop(shopId)
         .then((res: any) => {
           console.log(res);
-          setProducts(res.products);
+          const { products } = res;
+          const manageProductItems = products.map((product: product) => ({
+            isSelected: false,
+            product: { ...product },
+          }));
+          setProducts(manageProductItems);
           setIsLoading(false);
         })
         .catch(error => {
@@ -47,7 +59,53 @@ const ManageProductsPage: React.FC = () => {
         });
   }, [shopId]);
 
+  const handleSelectProduct = (id: string) => {
+    const newProducts = products
+      ? products.map(product => ({ ...product }))
+      : null;
+
+    const selectedProduct = _.find(
+      newProducts,
+      product => product.product._id === id,
+    );
+    if (!selectedProduct) return;
+    selectedProduct.isSelected = selectedProduct.isSelected ? false : true;
+
+    setProducts(newProducts);
+  };
+
+  const handleSelectAll = (e: ChangeEvent<HTMLInputElement>) => {
+    const { checked } = e.target;
+    const newProducts = products
+      ? products.map(product => ({
+          ...product,
+          isSelected: checked,
+        }))
+      : null;
+    setProducts(newProducts);
+  };
+
+  const handleDeleteProduct = async (id: string) => {
+    const prevProducts = products && products.map(product => ({ ...product }));
+    try {
+      const newProducts =
+        products && products.filter(product => product.product._id !== id);
+      setProducts(newProducts);
+      await productAPI.deleteProduct(id);
+    } catch (error) {
+      console.log(getErrorMessage(error));
+      setProducts(prevProducts);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    if (!products) return;
+    products.forEach(product => {
+      if (product.isSelected) handleDeleteProduct(product.product._id!);
+    });
+  };
   return isLoading ? (
+    //Loading
     <OneToFivePage>
       <div className={style.loadingWrapper}>
         <Spinner animation="border" variant="primary" />
@@ -55,50 +113,8 @@ const ManageProductsPage: React.FC = () => {
     </OneToFivePage>
   ) : (
     <OneToFivePage>
-      {/* <div className={style.wrapper}>
-          <div className={style.content}>
-            <div className={style.header}>
-              <div>
-                <h3 className={style.title}>
-                  <FormattedMessage id="ManageProductsPage.manageProductsTitle" />
-                </h3>
-                <h6>
-                  <FormattedMessage id="ManageProductsPage.manageProductsSubtitle" />
-                </h6>
-              </div>
-              <Link to="/products/add" className={style.addProductButton}>
-                <Button className="d-flex justify-content-center align-items-center">
-                  <BsPlusCircle className="me-2" />
-                  <FormattedMessage id="ManageProductsPage.addProductButton" />
-                </Button>
-              </Link>
-            </div>
-            
-            {!!(shopId && products && products.length > 0) ? (
-              <div className={style.productList}>
-                {products.map((product: product) => (
-                  <Product product={product} />
-                ))}
-              </div>
-            ) : (
-              <>
-                <div className={style.noProductsImage}>
-                  <img
-                    src={noProductsImage}
-                    alt="A picture telling there are no products"
-                  />
-                </div>
-                <h4 className={style.title}>
-                  <FormattedMessage id="ManageProductsPage.noProductsTitle" />
-                </h4>
-                <h6>
-                  <FormattedMessage id="ManageProductsPage.noProductsSubtitle" />
-                </h6>
-              </>
-            )}
-          </div>
-        </div> */}
       {!(shopId && products && products.length > 0) ? (
+        //Empty product
         <div className={style.wrapper}>
           <div className={style.content}>
             <div className={style.header}>
@@ -132,15 +148,11 @@ const ManageProductsPage: React.FC = () => {
           </div>
         </div>
       ) : (
-        //    <div className={style.productList}>
-        //    {products.map((product: product) => (
-        //      <Product product={product} />
-        //    ))}
-        //  </div>
-
+        //Manage Product
         <div className={style.wrapperK}>
           <div className={style.contentK}>
             <Container className={`p-0`}>
+              {/* Header */}
               <Row className={`bg-white p-4 border ${style.headerK}`}>
                 <Col lg={6}>
                   <div>
@@ -157,7 +169,7 @@ const ManageProductsPage: React.FC = () => {
                     <Form.Control placeholder="Search by title or ID..."></Form.Control>
                   </Form>
                 </Col>
-                <Col lg={3}>
+                <Col lg={3} className={`d-flex justify-content-end`}>
                   <Link to="/products/add" className={style.addProductButton}>
                     <Button className="d-flex justify-content-center align-items-center">
                       <BsPlusCircle className="me-2" />
@@ -166,178 +178,39 @@ const ManageProductsPage: React.FC = () => {
                   </Link>
                 </Col>
               </Row>
-              <Form>
-                {/* Control panel */}
-                <Row className={`mt-4`}>
-                  <div
-                    className={`border bg-white px-4 py-2 d-flex align-items-center ${style.controlPanelContainer}`}
-                  >
-                    <Form.Check inline type="checkbox" className={`me-5`} />
-                    <span>
-                      <Button variant="danger">Delete selected</Button>
-                    </span>
-                  </div>
-                </Row>
 
-                {/* {Product list} */}
-                <Row className={`mt-3`}>
-                  <div
-                    className={`border bg-white px-4 pt-1 pb-3 ${style.controlPanelContainer}`}
-                  >
-                    <Row className={`pe-4`}>
-                      <Col lg={1} className={`d-flex align-items-center`}>
-                        <Form.Check inline type="checkbox" className={`me-5`} />
-                      </Col>
-                      <Col lg={2} className={`d-flex align-items-center my-3`}>
-                        <img
-                          src={oneToOnePlaceholderImg}
-                          height={100}
-                          alt="Placeholder"
-                        />
-                      </Col>
-                      <Col
-                        lg={5}
-                        className={`d-flex align-items-center border-bottom `}
-                      >
-                        <span className={`fw-bold`}>
-                          The Ultimate Guide to Designing, Prototyping and Mass
-                          Manufacturing your Product Idea...
-                        </span>
-                      </Col>
-                      <Col
-                        lg={2}
-                        className={`d-flex align-items-center border-bottom `}
-                      >
-                        $99.99
-                      </Col>
-                      <Col
-                        lg={2}
-                        className={`d-flex align-items-center border-bottom  justify-content-around`}
-                      >
-                        <span className={`border ${style.statusBadgeActive}`}>
-                          Active
-                        </span>
-                        <div>
-                          <BsGearFill style={{ color: '#5c5c5c' }} />
-                          <BsCaretDownFill style={{ color: '#5c5c5c' }} />
-                        </div>
-                      </Col>
-                    </Row>
+              {/* Control panel */}
+              <Row className={`mt-4`}>
+                <div
+                  className={`border bg-white px-4 py-2 d-flex align-items-center ${style.controlPanelContainer}`}
+                >
+                  <Form.Check
+                    inline
+                    type="checkbox"
+                    className={`me-5`}
+                    checked={products.every(product => product.isSelected)}
+                    onChange={e => handleSelectAll(e)}
+                  />
+                  <span>
+                    <Button variant="danger" onClick={handleDeleteSelected}>
+                      Delete selected
+                    </Button>
+                  </span>
+                </div>
+              </Row>
 
-                    <Row className={`mt-4`}>
-                      <Col lg={{ offset: 3, span: 3 }}>
-                        <Row>
-                          <p className={``}>LAST 30 DAYS</p>
-                        </Row>
-                        <Row>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>views</span>
-                          </Col>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>sale</span>
-                          </Col>
-                        </Row>
-                      </Col>
-                      <Col lg={{ span: 3 }}>
-                        <Row>
-                          <p className={``}>ALL TIME</p>
-                        </Row>
-                        <Row>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>sale</span>
-                          </Col>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>$0.00</span>
-                            <span className={`text-muted`}>revenue</span>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </div>
-                </Row>
-
-                <Row className={``}>
-                  <div
-                    className={`border border-top-0 bg-white px-4 pt-1 pb-3 ${style.controlPanelContainer}`}
-                  >
-                    <Row className={`pe-4`}>
-                      <Col lg={1} className={`d-flex align-items-center`}>
-                        <Form.Check inline type="checkbox" className={`me-5`} />
-                      </Col>
-                      <Col lg={2} className={`d-flex align-items-center my-3`}>
-                        <img
-                          src={oneToOnePlaceholderImg}
-                          height={100}
-                          alt="Placeholder"
-                        />
-                      </Col>
-                      <Col
-                        lg={5}
-                        className={`d-flex align-items-center border-bottom `}
-                      >
-                        <span className={`fw-bold`}>
-                          The Ultimate Guide to Designing, Prototyping and Mass
-                          Manufacturing your Product Idea...
-                        </span>
-                      </Col>
-                      <Col
-                        lg={2}
-                        className={`d-flex align-items-center border-bottom `}
-                      >
-                        $99.99
-                      </Col>
-                      <Col
-                        lg={2}
-                        className={`d-flex align-items-center border-bottom  justify-content-around`}
-                      >
-                        <span className={`border ${style.statusBadgeActive}`}>
-                          Active
-                        </span>
-                        <div>
-                          <BsGearFill style={{ color: '#5c5c5c' }} />
-                          <BsCaretDownFill style={{ color: '#5c5c5c' }} />
-                        </div>
-                      </Col>
-                    </Row>
-
-                    <Row className={`mt-4`}>
-                      <Col lg={{ offset: 3, span: 3 }}>
-                        <Row>
-                          <p className={``}>LAST 30 DAYS</p>
-                        </Row>
-                        <Row>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>views</span>
-                          </Col>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>sale</span>
-                          </Col>
-                        </Row>
-                      </Col>
-                      <Col lg={{ span: 3 }}>
-                        <Row>
-                          <p className={``}>ALL TIME</p>
-                        </Row>
-                        <Row>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>0</span>
-                            <span className={`text-muted`}>sale</span>
-                          </Col>
-                          <Col className={`d-flex flex-column`}>
-                            <span className={`text-muted`}>$0.00</span>
-                            <span className={`text-muted`}>revenue</span>
-                          </Col>
-                        </Row>
-                      </Col>
-                    </Row>
-                  </div>
-                </Row>
-              </Form>
+              {/* {Product list} */}
+              <Row className={`mt-3 border bg-white`}>
+                {products.map(productItem => (
+                  <Product
+                    key={productItem.product._id!}
+                    isSelected={productItem.isSelected}
+                    product={productItem.product}
+                    onSelectProduct={handleSelectProduct}
+                    onDeleteProduct={handleDeleteProduct}
+                  />
+                ))}
+              </Row>
             </Container>
           </div>
         </div>
--- src/pages/ManageProductsPage/Product.tsx
+++ src/pages/ManageProductsPage/Product.tsx
@@ -1,20 +1,160 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { product } from "../../app/util/interfaces";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import { BsGearFill, BsCaretDownFill } from 'react-icons/bs';
+
+import { product } from '../../app/util/interfaces';
 
 import style from './ManageProductsPage.module.css';
 
 interface Props {
+  key: string;
+  isSelected: boolean;
   product: product;
+  onSelectProduct: Function;
+  onDeleteProduct: Function;
 }
 
-const Product: React.FC<Props> = (props) => {
-  const { product } = props;
+const Product: React.FC<Props> = props => {
+  const notAvailableImg =
+    'https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101031/112815934-no-image-available-icon-flat-vector-illustration.jpg?ver=6';
 
-  return <Link to={`/product/${product._id}`} className={style.productInList}>
-    {product.productName}
-  </Link>
-}
+  const { product, isSelected, onSelectProduct, onDeleteProduct } = props;
+
+  return (
+    <div className={`px-4 pt-1 pb-3 ${style.productWrapper}`}>
+      {/* Product description and status */}
+      <Row className={`pe-4`}>
+        {/* Checkbox */}
+        <Col lg={1} className={`d-flex align-items-center`}>
+          <Form.Check
+            inline
+            type="checkbox"
+            className={`me-5`}
+            checked={isSelected}
+            onChange={() => onSelectProduct(product._id)}
+          />
+        </Col>
+        {/* Product thumbnail */}
+        <Col lg={2} className={`d-flex align-items-center my-3`}>
+          <div className={`${style.productThumbnailWrapper}`}>
+            <img
+              src={
+                product.productPictures
+                  ? product.productPictures[0]
+                  : notAvailableImg
+              }
+              className={`${style.productThumbnail}`}
+              height={100}
+              alt="Placeholder"
+            />
+          </div>
+        </Col>
+        {/* Product name */}
+        <Col lg={5} className={`d-flex align-items-center border-bottom `}>
+          <Link to={`/product/${product._id}`} className={style.productInList}>
+            <span className={`fw-bold text-dark ${style.productName}`}>
+              {product.productName}
+            </span>
+          </Link>
+        </Col>
+        {/* Product price */}
+        <Col lg={2} className={`d-flex align-items-center border-bottom `}>
+          ${product.productPrice}
+        </Col>
+        {/* Settings dropdown menu */}
+        <Col
+          lg={2}
+          className={`d-flex align-items-center border-bottom  justify-content-around`}
+        >
+          <span
+            className={`border d-flex align-items-center ${style.statusBadge} ${
+              product.productStatus ? style.active : style.inactive
+            }`}
+          >
+            {product.productStatus ? 'Active' : 'Inactive'}
+          </span>
+          <div>
+            <div className="dropdown">
+              <div
+                id="settingsMenuDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                <BsGearFill style={{ color: '#5c5c5c' }} />
+                <BsCaretDownFill style={{ color: '#5c5c5c' }} />
+              </div>
+              <ul
+                className="dropdown-menu position-absolute mt-1"
+                aria-labelledby="settingsMenuDropdown"
+              >
+                <li>
+                  <Link to={`#`} className="dropdown-item">
+                    Edit
+                  </Link>
+                </li>
+                <li>
+                  <Link to={`#`} className="dropdown-item">
+                    Hide
+                  </Link>
+                </li>
+                <li>
+                  <span
+                    onClick={() => onDeleteProduct(product._id)}
+                    className="dropdown-item text-danger"
+                  >
+                    Delete
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </Col>
+      </Row>
+
+      {/* Product statistics */}
+      <Row className={`mt-4`}>
+        {/* Last 30 days */}
+        <Col lg={{ offset: 3, span: 3 }}>
+          <Row>
+            <p className={``}>LAST 30 DAYS</p>
+          </Row>
+          <Row>
+            <Col className={`d-flex flex-column`}>
+              <span className={`text-muted`}>0</span>
+              <span className={`text-muted`}>views</span>
+            </Col>
+            <Col className={`d-flex flex-column`}>
+              <span className={`text-muted`}>0</span>
+              <span className={`text-muted`}>sale</span>
+            </Col>
+          </Row>
+        </Col>
+        {/* All time */}
+        <Col lg={{ span: 3 }}>
+          <Row>
+            <p className={``}>ALL TIME</p>
+          </Row>
+          <Row>
+            <Col className={`d-flex flex-column`}>
+              <span className={`text-muted`}>{product.totalSold}</span>
+              <span className={`text-muted`}>sale</span>
+            </Col>
+            <Col className={`d-flex flex-column`}>
+              <span className={`text-muted`}>
+                ${product.totalSold * product.productPrice}
+              </span>
+              <span className={`text-muted`}>revenue</span>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Product;
