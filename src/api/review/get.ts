ca866b3c57f563ad83304d1f9012850bcaadfafe
Duc-Lam-jh <duclam227@gmail.com>
2022-05-14 20:29:45 +0700

    add review api

---
+++ src/api/review/get.ts
@@ -0,0 +1,21 @@
+import axiosClient from '..';
+import { getJwt } from '../../app/util/authHelpers';
+
+const REVIEW_ENDPOINT = 'api/v1/reviews';
+
+class Get {
+	getReviewsOfProductByPage (productId: string, pageNumber: number, perPage: number) {
+		const jwt = getJwt();
+		const config = {
+			headers: {
+				Authorization: `Bearer ${jwt}`,
+			},
+		};
+		return axiosClient.get(
+			`${REVIEW_ENDPOINT}/${productId}?page=${pageNumber}&limit=${perPage}`,
+			config,
+		);
+	}
+}
+
+export default new Get();
