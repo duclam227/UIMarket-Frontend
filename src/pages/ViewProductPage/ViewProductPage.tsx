8f512573088bc950316291c80374ba8adea17f75
Duc-Lam-jh <duclam227@gmail.com>
2022-05-14 20:30:24 +0700

    add reviews section

--- src/pages/ViewProductPage/ViewProductPage.tsx
+++ src/pages/ViewProductPage/ViewProductPage.tsx
@@ -5,26 +5,63 @@ import { useParams } from 'react-router-dom';
 
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { product } from '../../app/util/interfaces';
+import { getErrorMessage } from '../../app/util';
 import { PageWithNavbar } from '../../components';
 
 import productAPI from '../../api/product';
+import reviewAPI from '../../api/review';
 
-import style from './ViewProductPage.module.css';
 import SectionDescription from './SectionDescription';
 import SectionHeader from './SectionHeader';
 import SectionImages from './SectionImages';
+import SectionReviews from './SectionReviews';
+
+import style from './ViewProductPage.module.css';
+
+const ITEMS_PER_PAGE = 10;
 
 const ViewProductPage: React.FC = () => {
   const { id } = useParams();
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [product, setProduct] = useState<product | null>(null);
+  const [reviews, setReviews] = useState<Array<any> | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  const goToPage = (pageNumber: number) => {
+    setIsLoading(true);
+    reviewAPI
+      .getReviewsOfProductByPage(id!, pageNumber, ITEMS_PER_PAGE)
+      .then((res: any) => {
+        const { totalPages, reviews, page } = res;
+        setCurrentPage(page);
+        setReviews(reviews);
+        setTotalPages(totalPages);
+        setIsLoading(false);
+      })
+      .catch(error => {
+        const errorMsg = getErrorMessage(error);
+        console.log(errorMsg);
+        setIsLoading(false);
+      });
+  };
 
   useEffect(() => {
+    setIsLoading(true);
     productAPI
       .getProductById(id!)
       .then((res: any) => {
         setProduct(res.product);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    reviewAPI
+      .getReviewsOfProductByPage(id!, 1, 10)
+      .then((res: any) => {
+        setReviews(res.reviews);
         setIsLoading(false);
       })
       .catch(error => {
@@ -41,11 +78,15 @@ const ViewProductPage: React.FC = () => {
   ) : (
     <PageWithNavbar>
       <div className={style.wrapper}>
-        {/* <div className={style.content}> */}
         <SectionHeader title={product?.productName!} />
         <SectionImages images={product?.productPictures!} />
         <SectionDescription body={product?.productDescription!} />
-        {/* </div> */}
+        <SectionReviews
+          reviews={reviews!}
+          totalPages={totalPages}
+          currentPage={currentPage}
+          handleGoToPage={(page: number) => goToPage(page)}
+        />
       </div>
     </PageWithNavbar>
   );
