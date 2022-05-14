0b95da6d00fc6bd76cbdd8259c2d70689dfeda36
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-05-14 23:19:42 +0700

    optimize code

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,6 +1,5 @@
 import { FunctionComponent, useEffect, useState } from 'react';
 import { Button, Col, Row } from 'react-bootstrap';
-import { product } from '../../app/util/interfaces';
 import { PageWithNavbar } from '../../components';
 import Container from 'react-bootstrap/Container';
 
@@ -8,16 +7,11 @@ import { getErrorMessage } from '../../app/util/index';
 
 import styles from './CartPage.module.css';
 import cartAPI from '../../api/cart/index';
-import { State } from '../../redux/store';
-import { useSelector } from 'react-redux';
 
 const CartPage: FunctionComponent = () => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
   const [cartProducts, setProducts] = useState<Array<any>>([]);
-  const currentUser = useSelector((state: State) => state.auth.user);
-
-  const summary: any = { numOfItems: 3, totalCost: 100 };
 
   useEffect(() => {
     setIsLoading(true);
@@ -36,32 +30,33 @@ const CartPage: FunctionComponent = () => {
   }, []);
 
   const renderItems = (cart: Array<any>) => {
-    const handleRemoveItem = (item: any) => {
+    const handleRemoveItem = (removedItem: any) => {
+      const newList = cartProducts.filter(item => {
+        return item.product._id !== removedItem._id;
+      });
+      setProducts(newList);
+
       cartAPI
-        .removeSingleProduct(item._id)
-        .then((res: any) => {
-          console.log(res);
-        })
+        .removeSingleProduct(removedItem._id)
+        .then((res: any) => {})
         .catch(error => {
           const errorMsg = getErrorMessage(error);
           setError(errorMsg);
         });
     };
 
-    return cart.map((item: any, i: number) => {
+    return cart.map((item: any) => {
       const { product } = item;
+      const pLink = `./product/${product._id}`;
       return (
         <div
-          key={i}
+          key={product._id}
           className="d-flex flex-wrap flex-lg-nowrap align-items-stretch py-4 border-bottom border-1 mb-4"
         >
-          <a className={styles.productimg} href="#">
+          <a className={styles.productimg} href={pLink}>
             <img src={product.productPictures[0]} alt=".." />
           </a>
-          <div
-            className={'mw-50 text-truncate m-2 flex-fill'}
-            style={{ maxWidth: '60%' }}
-          >
+          <div className={'text-truncate m-2 flex-fill ' + styles.productTitle}>
             {product.productName}
           </div>
           <div className="flex-fill">
@@ -69,13 +64,12 @@ const CartPage: FunctionComponent = () => {
               <strong>${product.productPrice}</strong>
             </div>
             <div className="text-end">
-              <a
-                className="text-primary m-2"
-                href=""
+              <button
+                className="btn btn-link text-decoration-none"
                 onClick={() => handleRemoveItem(product)}
               >
                 Remove
-              </a>
+              </button>
             </div>
           </div>
         </div>
@@ -85,9 +79,12 @@ const CartPage: FunctionComponent = () => {
 
   const totalPrice = (cart: any) => {
     let price = 0;
-    for (let i = 0; i < cart.length; i++) {
-      price += cart[i].product.productPrice;
-    }
+
+    price = cart.reduce(
+      (previousValue: any, currentValue: any) =>
+        previousValue + currentValue.product.productPrice,
+      price,
+    );
     return price;
   };
 
