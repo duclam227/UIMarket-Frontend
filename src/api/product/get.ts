7540eb16673237bc13a8408bf37d30da43a4d1dd
KoahF <khoakg2013@gmail.com>
2022-05-15 22:29:29 +0700

    Add API for searching products

--- src/api/product/get.ts
+++ src/api/product/get.ts
@@ -3,25 +3,35 @@ import axiosClient from '..';
 const PRODUCT_ENDPOINT = 'api/v1/products';
 
 class Get {
-	getProductById = (id: string) => {
-		return axiosClient.get(`${PRODUCT_ENDPOINT}/info/${id}`);
-	};
+  getProductById = (id: string) => {
+    return axiosClient.get(`${PRODUCT_ENDPOINT}/info/${id}`);
+  };
 
-	getAllProductsByPageNumber = (pageNumber: number | string, itemsPerPage: number) => {
-		return axiosClient.get(`${PRODUCT_ENDPOINT}?page=${pageNumber}&limit=${itemsPerPage}`);
-	};
+  getAllProductsByPageNumber = (pageNumber: number | string, itemsPerPage: number) => {
+    return axiosClient.get(
+      `${PRODUCT_ENDPOINT}?page=${pageNumber}&limit=${itemsPerPage}`,
+    );
+  };
 
-	getTrendingProducts = (pageNumber: number | string, itemsPerPage: number) => {
-		return axiosClient.get(
-			`${PRODUCT_ENDPOINT}?page=${pageNumber}&limit=${itemsPerPage}&sort=sort-des`,
-		);
-	};
+  getTrendingProducts = (pageNumber: number | string, itemsPerPage: number) => {
+    return axiosClient.get(
+      `${PRODUCT_ENDPOINT}?page=${pageNumber}&limit=${itemsPerPage}&sort=sort-des`,
+    );
+  };
 
-	searchProduct = (query: string, pageNumber: number | string, itemsPerPage: number) => {
-		return axiosClient.get(
-			`${PRODUCT_ENDPOINT}/search/${query}?page=${pageNumber}&limit=${itemsPerPage}`,
-		);
-	};
+  searchProduct = (
+    query: string,
+    pageNumber: number | string,
+    itemsPerPage: number,
+    filter?: string | null,
+    sort?: string | null,
+  ) => {
+    return axiosClient.get(
+      `${PRODUCT_ENDPOINT}/search/${query}?page=${pageNumber}&limit=${itemsPerPage}${
+        filter ? `&filter=${filter}` : ''
+      }${sort ? `&sort=${sort}` : ''}`,
+    );
+  };
 }
 
 export default new Get();
