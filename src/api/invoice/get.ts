37dffdeac2443ee6989c0b8dea0d1ad25fb36371
Duc-Lam-jh <duclam227@gmail.com>
2022-05-19 19:11:41 +0700

    add api to get invoice

---
+++ src/api/invoice/get.ts
@@ -0,0 +1,22 @@
+import axiosClient from '..';
+import { getJwt } from '../../app/util/authHelpers';
+
+const INVOICE_ENDPOINT = 'api/v1/invoices';
+
+class Get {
+	getPurchaseHistoryByPage = (pageNumber: string | number, perPage: string | number) => {
+		const jwt = getJwt();
+		const config = {
+			headers: {
+				Authorization: `Bearer ${jwt}`,
+			},
+		};
+
+		return axiosClient.get(
+			`${INVOICE_ENDPOINT}/history?page=${pageNumber}&limit=${perPage}`,
+			config,
+		);
+	};
+}
+
+export default new Get();
