2b5d0ed8a61023de1a18705f13e754c97fd8d743
KoahF <khoakg2013@gmail.com>
2022-05-20 15:40:31 +0700

    Add report button to threedot menu of Comment

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,98 +1,127 @@
-import { ChangeEvent, FC, useEffect, useState } from "react";
-import { Button, Form, Modal } from "react-bootstrap";
-import { FormattedMessage, IntlShape, injectIntl } from "react-intl";
-import JsxParser from "react-jsx-parser";
+import { ChangeEvent, FC, useEffect, useState } from 'react';
+import { Button, Form, Modal } from 'react-bootstrap';
+import { FormattedMessage, IntlShape, injectIntl } from 'react-intl';
+import JsxParser from 'react-jsx-parser';
 import parse from 'html-react-parser';
-import { customer, voteStatus } from "../../app/util/interfaces";
-import answerAPI from "../../api/answer";
+import { customer, voteStatus } from '../../app/util/interfaces';
+import answerAPI from '../../api/answer';
 
-import { Paginator, ThreeDotMenu } from "..";
+import { Paginator, ReportModal, ThreeDotMenu } from '..';
 
-import SectionEditComment from "./SectionEditComment";
+import SectionEditComment from './SectionEditComment';
 
 import style from './Comment.module.css';
-import commentAPI from "../../api/comment";
+import commentAPI from '../../api/comment';
 
 interface Props {
   answer: any;
   currentUser: customer | null;
   question: any;
   comment: any;
-  intl: IntlShape
+  intl: IntlShape;
 }
 
-const Comment: FC<Props> = (props) => {
+const Comment: FC<Props> = props => {
   const { answer, currentUser, question, comment, intl } = props;
 
-  const [commentContent, setCommentContent] = useState<string>(comment.commentContent || '');
+  const [commentContent, setCommentContent] = useState<string>(
+    comment.commentContent || '',
+  );
   const [isEdit, setIsEdit] = useState<boolean>(false);
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [showReportModal, setShowReportModal] = useState<boolean>(false);
+  const handleShowReportModal = () => {
+    setShowReportModal(true);
+  };
+  const handleCloseReportModal = () => {
+    setShowReportModal(false);
+  };
 
   if (!comment || !commentContent) {
     return null;
   }
 
   const isUserAuthenticated = !!currentUser;
-  const isCurrentUserAuthor = isUserAuthenticated && currentUser.customerEmail === comment.userId.customerEmail;
+  const isCurrentUserAuthor =
+    isUserAuthenticated && currentUser.customerEmail === comment.userId.customerEmail;
 
   const editComment = () => {
     setIsEdit(true);
-  }
+  };
 
   const handleSaveComment = (newContent: string) => {
-    commentAPI.updateComment(newContent, comment._id)
+    commentAPI
+      .updateComment(newContent, comment._id)
       .then(res => {
         console.log(res);
         setCommentContent(newContent);
       })
       .catch(error => {
         console.log(error);
-      })
-  }
+      });
+  };
 
   const deleteComment = () => {
     setIsModalOpen(true);
-  }
+  };
 
-  const menuItems = [
+  const authorMenuItems = [
     {
       key: 'edit',
       label: intl.formatMessage({ id: 'Comment.editLabel' }),
-      function: editComment
+      function: editComment,
     },
     {
       key: 'delete',
       label: intl.formatMessage({ id: 'Comment.deleteLabel' }),
-      function: deleteComment
+      function: deleteComment,
     },
-  ]
+  ];
+  const reportMenuItem = {
+    key: 'report',
+    label: intl.formatMessage({ id: 'Answer.reportLabel' }),
+    function: handleShowReportModal,
+  };
+  const menuItems = isCurrentUserAuthor
+    ? [reportMenuItem, ...authorMenuItems]
+    : [reportMenuItem];
 
   const confirmDeleteModal = (
     <Modal show={isModalOpen} onHide={() => setIsModalOpen(false)}>
       <Modal.Header closeButton>
-        <Modal.Title><FormattedMessage id='Comment.confirmDelete' /></Modal.Title>
+        <Modal.Title>
+          <FormattedMessage id="Comment.confirmDelete" />
+        </Modal.Title>
       </Modal.Header>
 
       <Modal.Body>
-        <p><FormattedMessage id='Comment.confirmDeleteContent' /></p>
+        <p>
+          <FormattedMessage id="Comment.confirmDeleteContent" />
+        </p>
       </Modal.Body>
 
       <Modal.Footer>
-        <Button variant="secondary" onClick={() => setIsModalOpen(false)}><FormattedMessage id='Comment.close' /></Button>
-        <Button variant="primary" onClick={() => {
-          commentAPI.deleteComment(comment._id)
-            .then((res: any) => {
-              setCommentContent('');
-            })
-            .catch(error => {
-              console.log(error);
-            })
-        }}>
-          <FormattedMessage id='Comment.deleteLabel' />
+        <Button variant="secondary" onClick={() => setIsModalOpen(false)}>
+          <FormattedMessage id="Comment.close" />
+        </Button>
+        <Button
+          variant="primary"
+          onClick={() => {
+            commentAPI
+              .deleteComment(comment._id)
+              .then((res: any) => {
+                setCommentContent('');
+              })
+              .catch(error => {
+                console.log(error);
+              });
+          }}
+        >
+          <FormattedMessage id="Comment.deleteLabel" />
         </Button>
       </Modal.Footer>
     </Modal>
-  )
+  );
 
   return (
     <div className={style.comment}>
@@ -101,21 +130,26 @@ const Comment: FC<Props> = (props) => {
       <div className={style.content}>
         <div className={style.authorInfo}>
           {comment.userId.customerName}
-          {isCurrentUserAuthor &&
-            <ThreeDotMenu menuItems={menuItems} />
-          }
+          <ThreeDotMenu menuItems={menuItems} />
         </div>
-        {isEdit
-          ? <SectionEditComment
+        {isEdit ? (
+          <SectionEditComment
             initialValue={commentContent}
             onHide={() => setIsEdit(false)}
             onSave={(content: string) => handleSaveComment(content)}
           />
-          : commentContent
-        }
+        ) : (
+          commentContent
+        )}
+        <ReportModal
+          show={showReportModal}
+          onClose={handleCloseReportModal}
+          reportObjectId={comment._id!}
+          type="comment"
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default injectIntl(Comment);
