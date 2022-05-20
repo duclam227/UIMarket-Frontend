9fa8b45d04e6773de2fa9e82b2a046ba9c20be3b
KoahF <khoakg2013@gmail.com>
2022-05-20 20:38:47 +0700

    Add translations

--- src/components/ReportModal/ReportModal.tsx
+++ src/components/ReportModal/ReportModal.tsx
@@ -1,6 +1,7 @@
 import { FC, ChangeEvent, useState } from 'react';
 
 import { toast } from 'react-toastify';
+import { FormattedMessage, IntlShape, injectIntl } from 'react-intl';
 
 import Container from 'react-bootstrap/Container';
 import Modal from 'react-bootstrap/Modal';
@@ -15,49 +16,78 @@ interface Props {
   onClose: Function;
   reportObjectId: string;
   type: 'product' | 'question' | 'answer' | 'comment';
+  intl: IntlShape;
 }
 
 const ReportModal: FC<Props> = props => {
-  const { show, onClose, type, reportObjectId } = props;
+  const { show, onClose, type, reportObjectId, intl } = props;
+
+  const reportModalTitle = <FormattedMessage id="ReportModal.reportModalTitle" />;
+
+  const reasonSelectPlaceholder = intl.formatMessage({
+    id: 'ReportModal.reasonSelectPlaceholder',
+  });
+  const productTypeLabel = intl.formatMessage({ id: 'ReportModal.productTypeLabel' });
+  const questionTypeLabel = intl.formatMessage({ id: 'ReportModal.questionTypeLabel' });
+  const answerTypeLabel = intl.formatMessage({ id: 'ReportModal.answerTypeLabel' });
+  const commentTypeLabel = intl.formatMessage({ id: 'ReportModal.commentTypeLabel' });
+
+  const objectionableContentReasonLabel = intl.formatMessage({
+    id: 'ReportModal.objectionableContentReasonLabel',
+  });
+  const copyrightViolationReasonLabel = intl.formatMessage({
+    id: 'ReportModal.copyrightViolationReasonLabel',
+  });
+
+  const emptyReasonToastLabel = intl.formatMessage({
+    id: 'ReportModal.emptyReasonToastLabel',
+  });
+  const successToastLabel = intl.formatMessage({
+    id: 'ReportModal.successToastLabel',
+  });
+
+  const closeBtnLabel = <FormattedMessage id="ReportModal.closeBtnLabel" />;
+  const sendReportBtnLabel = <FormattedMessage id="ReportModal.sendReportBtnLabel" />;
+
   const [reportReason, setReportReason] = useState<string | null>(null);
   const reportTypeContent = {
     product: {
-      label: 'Product',
+      label: productTypeLabel,
       reasons: [
         {
           key: 'objectionableContent',
-          label: 'Objectionable content',
+          label: objectionableContentReasonLabel,
         },
         {
           key: 'copyrightViolation',
-          label: 'Copyright violation',
+          label: copyrightViolationReasonLabel,
         },
       ],
     },
     question: {
-      label: 'Question',
+      label: questionTypeLabel,
       reasons: [
         {
           key: 'objectionableContent',
-          label: 'Objectionable content',
+          label: objectionableContentReasonLabel,
         },
       ],
     },
     answer: {
-      label: 'Answer',
+      label: answerTypeLabel,
       reasons: [
         {
           key: 'objectionableContent',
-          label: 'Objectionable content',
+          label: objectionableContentReasonLabel,
         },
       ],
     },
     comment: {
-      label: 'Comment',
+      label: commentTypeLabel,
       reasons: [
         {
           key: 'objectionableContent',
-          label: 'Objectionable content',
+          label: objectionableContentReasonLabel,
         },
       ],
     },
@@ -69,22 +99,25 @@ const ReportModal: FC<Props> = props => {
 
   const handleSubmitReport = async () => {
     if (!reportReason) {
-      toast.error('Please select a report reason');
+      toast.error(emptyReasonToastLabel);
       return;
     }
     try {
       await reportAPI.submitReport(reportObjectId, reportReason, type);
-      toast.success('Your report has been sent');
+      toast.success(successToastLabel);
       onClose();
     } catch (e) {
       toast.error(getErrorMessage(e));
     }
   };
+
   return (
     <Modal show={show} onHide={() => onClose()} centered>
       <Form>
         <Modal.Header closeButton>
-          <Modal.Title>Report This {reportTypeContent[type].label} </Modal.Title>
+          <Modal.Title>
+            {reportModalTitle} {reportTypeContent[type].label}{' '}
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Container>
@@ -93,7 +126,7 @@ const ReportModal: FC<Props> = props => {
               defaultValue="placeholder"
             >
               <option disabled value="placeholder">
-                Please select a report reason
+                {reasonSelectPlaceholder}
               </option>
               {reportTypeContent[type].reasons.map((reason: any) => (
                 <option key={reason.label} value={reason.label}>
@@ -105,10 +138,10 @@ const ReportModal: FC<Props> = props => {
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => onClose()}>
-            Close
+            {closeBtnLabel}
           </Button>
           <Button variant="primary" onClick={() => handleSubmitReport()}>
-            Send report
+            {sendReportBtnLabel}
           </Button>
         </Modal.Footer>
       </Form>
@@ -116,7 +149,7 @@ const ReportModal: FC<Props> = props => {
   );
 };
 
-export default ReportModal;
+export default injectIntl(ReportModal);
 
 // interface ReportInformation {
 //   explanation: string;
