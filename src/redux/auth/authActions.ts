b3ae76dd7417d5a6692dac3f80ac2b364e51cc6a
Duc-Lam-jh <duclam227@gmail.com>
2022-05-21 22:05:52 +0700

    save token when loggin in from google

--- src/redux/auth/authActions.ts
+++ src/redux/auth/authActions.ts
@@ -7,124 +7,125 @@ import { actionTypes } from './authActionTypes';
 import { Action } from './authInterfaces';
 
 const loginInProgress = () => {
-  return {
-    type: actionTypes.AUTH_LOGIN_REQUEST,
-  } as Action;
+	return {
+		type: actionTypes.AUTH_LOGIN_REQUEST,
+	} as Action;
 };
 
 const loginSuccess = (userData: Object) => {
-  return {
-    type: actionTypes.AUTH_LOGIN_SUCCESS,
-    user: { ...userData },
-  } as Action;
+	return {
+		type: actionTypes.AUTH_LOGIN_SUCCESS,
+		user: { ...userData },
+	} as Action;
 };
 
 const signUpInProgress = () => {
-  return {
-    type: actionTypes.AUTH_LOGIN_REQUEST,
-  } as Action;
+	return {
+		type: actionTypes.AUTH_LOGIN_REQUEST,
+	} as Action;
 };
 
 const signUpSuccess = (userData: Object) => {
-  return {
-    type: actionTypes.AUTH_SIGNUP_SUCCESS,
-    user: { ...userData },
-  } as Action;
+	return {
+		type: actionTypes.AUTH_SIGNUP_SUCCESS,
+		user: { ...userData },
+	} as Action;
 };
 
 const setUserToNull = () => {
-  return {
-    type: actionTypes.AUTH_USER_SIGN_OUT,
-  } as Action;
+	return {
+		type: actionTypes.AUTH_USER_SIGN_OUT,
+	} as Action;
 };
 
 const setError = (msg: string | null) => {
-  return {
-    type: actionTypes.AUTH_ERROR,
-    error: msg,
-  } as Action;
+	return {
+		type: actionTypes.AUTH_ERROR,
+		error: msg,
+	} as Action;
 };
 
 const logIn = (credentials: authCredentials) => {
-  return (dispatch: Dispatch<Action>) => {
-    dispatch(setError(null));
-    dispatch(loginInProgress());
-    authAPI
-      .logIn(credentials)
-      .then((res: any) => {
-        const { user, token } = res;
-        localStorage.setItem('authToken', token);
-        const customer: customer = { ...user };
-        dispatch(loginSuccess({ ...customer }));
-      })
-      .catch(error => {
-        console.log('Login error redux', error);
-        const errorMsg = getErrorMessage(error);
-        dispatch(setError(errorMsg));
-      });
-  };
+	return (dispatch: Dispatch<Action>) => {
+		dispatch(setError(null));
+		dispatch(loginInProgress());
+		authAPI
+			.logIn(credentials)
+			.then((res: any) => {
+				const { user, token } = res;
+				localStorage.setItem('authToken', token);
+				const customer: customer = { ...user };
+				dispatch(loginSuccess({ ...customer }));
+			})
+			.catch(error => {
+				console.log('Login error redux', error);
+				const errorMsg = getErrorMessage(error);
+				dispatch(setError(errorMsg));
+			});
+	};
 };
 
 const logInWithJWT = (jwt: string) => {
-  return (dispatch: Dispatch<Action>) => {
-    dispatch(setError(null));
-    dispatch(loginInProgress());
-    authAPI
-      .getUserFromToken(jwt)
-      .then((res: any) => {
-        const { user } = res;
-        const customer: customer = { ...user };
-        dispatch(loginSuccess({ ...customer }));
-      })
-      .catch(error => {
-        const errorMsg = getErrorMessage(error);
-        dispatch(setError(errorMsg));
-      });
-  };
+	return (dispatch: Dispatch<Action>) => {
+		dispatch(setError(null));
+		dispatch(loginInProgress());
+		authAPI
+			.getUserFromToken(jwt)
+			.then((res: any) => {
+				const { user } = res;
+				const customer: customer = { ...user };
+				dispatch(loginSuccess({ ...customer }));
+			})
+			.catch(error => {
+				const errorMsg = getErrorMessage(error);
+				dispatch(setError(errorMsg));
+			});
+	};
 };
 
 const logInWithGoogle = (tokenId: string) => {
-  return (dispatch: Dispatch<Action>) => {
-    dispatch(setError(null));
-    dispatch(loginInProgress());
-    authAPI
-      .logInWithGoogle(tokenId)
-      .then((res: any) => {
-        const { user } = res;
-        const customer: customer = { ...user };
-        dispatch(loginSuccess({ ...customer }));
-      })
-      .catch(error => {
-        const errorMsg = getErrorMessage(error);
-        dispatch(setError(errorMsg));
-      });
-  };
+	return (dispatch: Dispatch<Action>) => {
+		dispatch(setError(null));
+		dispatch(loginInProgress());
+		authAPI
+			.logInWithGoogle(tokenId)
+			.then((res: any) => {
+				const { user, accessToken } = res;
+				const customer: customer = { ...user };
+				localStorage.setItem('authToken', accessToken);
+				dispatch(loginSuccess({ ...customer }));
+			})
+			.catch(error => {
+				const errorMsg = getErrorMessage(error);
+				dispatch(setError(errorMsg));
+			});
+	};
 };
 
 const signUp = (credentials: authCredentials) => {
-  return (dispatch: Dispatch<Action>) => {
-    dispatch(setError(null));
-    dispatch(signUpInProgress());
-    authAPI
-      .signUp(credentials)
-      .then((res: any) => {
-        const { user, token } = res;
-        localStorage.setItem('authToken', token);
-        const customer: customer = { ...user };
-        dispatch(signUpSuccess({ ...customer }));
-      })
-      .catch(error => {
-        const errorMsg = getErrorMessage(error);
-        dispatch(setError(errorMsg));
-      });
-  };
+	return (dispatch: Dispatch<Action>) => {
+		dispatch(setError(null));
+		dispatch(signUpInProgress());
+		authAPI
+			.signUp(credentials)
+			.then((res: any) => {
+				const { user, token } = res;
+				localStorage.setItem('authToken', token);
+				const customer: customer = { ...user };
+				dispatch(signUpSuccess({ ...customer }));
+			})
+			.catch(error => {
+				const errorMsg = getErrorMessage(error);
+				dispatch(setError(errorMsg));
+			});
+	};
 };
 
 const logOut = () => {
-  return (dispatch: Dispatch<Action>) => {
-    localStorage.clear();
-    dispatch(setUserToNull());
-  };
+	return (dispatch: Dispatch<Action>) => {
+		localStorage.clear();
+		dispatch(setUserToNull());
+	};
 };
 
 export { signUp, logIn, logInWithJWT, logInWithGoogle, logOut, loginSuccess, setError };
