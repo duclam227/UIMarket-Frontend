d58acf5802d115b87545463b1101122a0bffe350
KoahF <khoakg2013@gmail.com>
2022-05-23 20:47:18 +0700

    Move sort and filter to sidebar

--- src/pages/ProductListPage/ProductListPage.tsx
+++ src/pages/ProductListPage/ProductListPage.tsx
@@ -13,6 +13,7 @@ import productAPI from '../../api/product/index';
 import { getErrorMessage } from '../../app/util/index';
 import { ProductList, PageWithNavbar, Paginator } from '../../components';
 import style from './ProductListPage.module.css';
+import Sidebar from './Sidebar';
 
 const ITEMS_PER_PAGE = 20;
 
@@ -30,60 +31,6 @@ const ProductListPage: FC<Props> = props => {
     />
   );
 
-  //Sort
-  const sortBySelectLabel = <FormattedMessage id="ProductListPage.sortBySelectLabel" />;
-  const sortOptionNameAscLabel = intl.formatMessage({
-    id: 'ProductListPage.sortOptionNameAsc',
-  });
-  const sortOptionNameDesLabel = intl.formatMessage({
-    id: 'ProductListPage.sortOptionNameDes',
-  });
-  const sortOptionPriceAscLabel = intl.formatMessage({
-    id: 'ProductListPage.sortOptionPriceAsc',
-  });
-  const sortOptionPriceDesLabel = intl.formatMessage({
-    id: 'ProductListPage.sortOptionPriceDes',
-  });
-  //Filter
-  const priceFilterSelectLabel = <FormattedMessage id="ProductListPage.priceFilterSelectLabel" />;
-  const priceFilterOptionAllLabel = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOptionAll',
-  });
-  const priceFilterOptionFreeLabel = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOptionFree',
-  });
-  const priceFilterOption019Label = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOption019',
-  });
-  const priceFilterOption2039Label = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOption2039',
-  });
-  const priceFilterOption4059Label = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOption4059',
-  });
-  const priceFilterOption6079Label = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOption6079',
-  });
-  const priceFilterOption80Label = intl.formatMessage({
-    id: 'ProductListPage.priceFilterOption80',
-  });
-
-  const sortOptions = [
-    { value: 'name-asc', label: sortOptionNameAscLabel },
-    { value: 'name-des', label: sortOptionNameDesLabel },
-    { value: 'money-asc', label: sortOptionPriceAscLabel },
-    { value: 'money-des', label: sortOptionPriceDesLabel },
-  ];
-
-  const filterOptions = [
-    { value: '', label: priceFilterOptionAllLabel },
-    { value: 'money-free', label: priceFilterOptionFreeLabel },
-    { value: 'money-0-19', label: priceFilterOption019Label },
-    { value: 'money-20-39', label: priceFilterOption2039Label },
-    { value: 'money-40-59', label: priceFilterOption4059Label },
-    { value: 'money-60-79', label: priceFilterOption6079Label },
-    { value: 'money-80', label: priceFilterOption80Label },
-  ];
 
   const [searchParams, setSearchParams] = useSearchParams();
   const searchKeyword = searchParams.get('keyword');
@@ -170,62 +117,65 @@ const ProductListPage: FC<Props> = props => {
         setIsLoading(false);
       });
   };
-  const handleContentControlChange = (e: ChangeEvent<HTMLSelectElement>) => {
-    const { name, value } = e.currentTarget;
-    searchParams.set(name, value);
-    setSearchParams(searchParams.toString());
-  };
   return (
     <PageWithNavbar>
-      <Container>
-        {searchKeyword ? (
-          <Row className={`mt-3`}>
-            <h5>
-              {searchResultMessage} "
-              <span className={style.searchQuery}>{searchKeyword}</span>"
-            </h5>
-          </Row>
-        ) : null}
-
-        {/* Filter & sort bar */}
-        <Row className={`${style.contentControlBar} pt-3 pb-1 w-100`}>
-          <Col lg={{ span: 3 }} className={`d-flex align-items-center`}>
-            <h6 className="text-nowrap me-3 p-0 m-0">{sortBySelectLabel} </h6>
-            <Form.Select name="sort" onChange={e => handleContentControlChange(e)}>
-              {sortOptions.map(option => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </Form.Select>
+      <Container fluid>
+        <Row>
+          <Col className={`p-0`} md={2}>
+            <Sidebar />
           </Col>
-          <Col lg={{ span: 3 }} className={`d-flex align-items-center`}>
-            <h6 className="text-nowrap me-3 p-0 m-0">Price range: </h6>
-            <Form.Select name="filter" onChange={e => handleContentControlChange(e)}>
-              {filterOptions.map(option => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </Form.Select>
+          <Col md={10}>
+            {searchKeyword ? (
+              <Row className={`mt-3`}>
+                <h5>
+                  {searchResultMessage} "
+                  <span className={style.searchQuery}>{searchKeyword}</span>"
+                </h5>
+              </Row>
+            ) : null}
+
+            {isLoading ? (
+              <Row className={`mt-5 d-flex justify-content-center`}>
+                <Spinner animation="border" />
+              </Row>
+            ) : (
+              <ProductList productList={products} />
+            )}
+
+            <Paginator
+              totalNumberOfPages={totalPages}
+              currentPage={currentPage}
+              handleClickGoToPage={(number: number) => goToPage(number)}
+            />
           </Col>
         </Row>
-        {isLoading ? (
-          <Row className={`mt-5 d-flex justify-content-center`}>
-            <Spinner animation="border" />
-          </Row>
-        ) : (
-          <ProductList productList={products} />
-        )}
-
-        <Paginator
-          totalNumberOfPages={totalPages}
-          currentPage={currentPage}
-          handleClickGoToPage={(number: number) => goToPage(number)}
-        />
       </Container>
     </PageWithNavbar>
   );
 };
 
 export default injectIntl(ProductListPage);
+
+/* Filter & sort bar */
+/* <Row className={`${style.contentControlBar} pt-3 pb-1 w-100`}>
+              <Col lg={{ span: 3 }} className={`d-flex align-items-center`}>
+                <h6 className="text-nowrap me-3 p-0 m-0">{sortBySelectLabel} </h6>
+                <Form.Select name="sort" onChange={e => handleContentControlChange(e)}>
+                  {sortOptions.map(option => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Col>
+              <Col lg={{ span: 3 }} className={`d-flex align-items-center`}>
+                <h6 className="text-nowrap me-3 p-0 m-0">Price range: </h6>
+                <Form.Select name="filter" onChange={e => handleContentControlChange(e)}>
+                  {filterOptions.map(option => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </Form.Select>
+              </Col>
+            </Row> */
