d029820ab5a288ee00d5973bd4ef9f58280ca416
Duc-Lam-jh <duclam227@gmail.com>
2022-05-30 19:45:23 +0700

    adjust RequireUser and Anonymous middleware

--- src/components/common/RequireUser/RequireUser.tsx
+++ src/components/common/RequireUser/RequireUser.tsx
@@ -1,11 +1,14 @@
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useLocation, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { State } from '../../../redux/store';
+import { getJwt } from '../../../app/util/authHelpers';
 
 const RequireUser = ({ children }: { children: JSX.Element }) => {
   const location = useLocation();
   const currentUser = useSelector((state: State) => state.auth.user);
-  if (!currentUser) {
+  const jwt = getJwt();
+
+  if (!currentUser && !jwt) {
     return <Navigate to="/login" state={{ from: location }} />;
   }
   return children;
