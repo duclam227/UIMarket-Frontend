69d8111fefce0fb77f13fe910390a693bd84477e
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-06-02 00:37:23 +0700

    api and upload info, banner

--- src/api/shop/get.ts
+++ src/api/shop/get.ts
@@ -4,15 +4,25 @@ import { getJwt } from '../../app/util/authHelpers';
 const SHOP_ENDPOINT = 'api/v1/shop';
 
 class Get {
-	getAllProductsOfShop (shopId: string) {
-		const jwt = getJwt();
-		const config = {
-			headers: {
-				Authorization: `Bearer ${jwt}`,
-			},
-		};
-		return axiosClient.get(`${SHOP_ENDPOINT}/product`, config);
-	}
+  getAllProductsOfShop(shopId: string) {
+    const jwt = getJwt();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    return axiosClient.get(`${SHOP_ENDPOINT}/product`, config);
+  }
+
+  getShopInfo(shopId: string) {
+    const jwt = getJwt();
+    const config = {
+      headers: {
+        Authorization: `Bearer ${jwt}`,
+      },
+    };
+    return axiosClient.get(`${SHOP_ENDPOINT}/info/${shopId}`, config);
+  }
 }
 
 export default new Get();
--- src/api/shop/index.ts
+++ src/api/shop/index.ts
@@ -1,10 +1,12 @@
 import get from './get';
 import post from './post';
+import put from './put';
 
 const shopAPI = {
-	getAllProductsOfShop: get.getAllProductsOfShop,
-
-	createShop: post.createShop,
+  getAllProductsOfShop: get.getAllProductsOfShop,
+  getShopInfo: get.getShopInfo,
+  createShop: post.createShop,
+  updateShop: put.updateShop,
 };
 
 export default shopAPI;
