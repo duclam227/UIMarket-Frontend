931dea568431d8370b8e788a03c82b18f638cba7
KoahF <khoakg2013@gmail.com>
2022-06-01 22:00:42 +0700

    Add Column types

--- src/app/util/interfaces.ts
+++ src/app/util/interfaces.ts
@@ -1,85 +1,102 @@
+import { any, string } from 'joi';
+import { FormattedMessage } from 'react-intl';
 export interface question {
-	title: string;
-	body: string;
-	tags: string[];
-	bounty: number;
-	question: string;
+  title: string;
+  body: string;
+  tags: string[];
+  bounty: number;
+  question: string;
 }
 
 export interface navigatorTabItem {
-	path: string;
-	label: string;
+  path: string;
+  label: string;
 }
 
 export interface authCredentials {
-	customerEmail: string;
-	customerPassword: string;
-	customerName?: string;
+  customerEmail: string;
+  customerPassword: string;
+  customerName?: string;
 }
 
 export interface PaginatorProps {
-	totalNumberOfPages: number;
-	currentPage: number;
-	handleClickGoToPage: Function;
+  totalNumberOfPages: number;
+  currentPage: number;
+  handleClickGoToPage: Function;
 }
 
 export interface voteStatus {
-	upvote: boolean;
-	downvote: boolean;
+  upvote: boolean;
+  downvote: boolean;
 }
 
 export interface PaginatorProps {
-	totalNumberOfPages: number;
-	currentPage: number;
-	handleClickGoToPage: Function;
+  totalNumberOfPages: number;
+  currentPage: number;
+  handleClickGoToPage: Function;
 }
 
 export interface customer {
-	_id: string;
-	customerName: string;
-	customerEmail: string;
-	customerPassword: string;
-	customerPhone: string;
-	customerDOB: string;
-	authToken: {
-		Google: string;
-	};
-	customerWallet: {
-		coin: string;
-		point: string;
-	};
-	customerStatus: number;
-	createdAt: Date;
-	updatedAt: Date;
-	shopId: string;
-	refreshtoken: string;
-	customerAvatar: string;
-	isAdmin: boolean;
+  _id: string;
+  customerName: string;
+  customerEmail: string;
+  customerPassword: string;
+  customerPhone: string;
+  customerDOB: string;
+  authToken: {
+    Google: string;
+  };
+  customerWallet: {
+    coin: string;
+    point: string;
+  };
+  customerStatus: number;
+  createdAt: Date;
+  updatedAt: Date;
+  shopId: string;
+  refreshtoken: string;
+  customerAvatar: string;
+  isAdmin: boolean;
 }
 export interface product {
-	_id?: string;
-	coverPicture?: string;
-	productName: string;
-	productPrice: number;
-	productCategory: string;
-	productDescription: string;
-	productPictures?: Array<string>;
-	productFile?: string;
-	productStatus?: number;
-	productId?: string;
-	totalSold?: number;
-	totalReview?: number;
-	productRating?: number;
-	shopId?: any;
-	shop?: any;
+  _id?: string;
+  coverPicture?: string;
+  productName: string;
+  productPrice: number;
+  productCategory: string;
+  productDescription: string;
+  productPictures?: Array<string>;
+  productFile?: string;
+  productStatus?: number;
+  productId?: string;
+  totalSold?: number;
+  totalReview?: number;
+  productRating?: number;
+  shopId?: any;
+  shop?: any;
 }
 
 export interface voteStatus {
-	upvote: boolean;
-	downvote: boolean;
+  upvote: boolean;
+  downvote: boolean;
 }
 export interface ReportObject {
   reportObject: string;
   reason: string;
   objectType: string;
 }
+export interface DataColumn {
+  path: string;
+  label: string;
+  width?: number;
+  bold?: boolean;
+}
+
+export interface InteractionColumn {
+  key: string;
+  label?: string;
+  width?: number;
+  content: Function;
+}
+
+export type Column = DataColumn | InteractionColumn;
