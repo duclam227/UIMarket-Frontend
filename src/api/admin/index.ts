673c34b962493875146062778852bf29df2f13e3
KoahF <khoakg2013@gmail.com>
2022-06-02 16:52:39 +0700

    add admin shops API

--- src/api/admin/index.ts
+++ src/api/admin/index.ts
@@ -2,8 +2,11 @@ import get from './get';
 import put from './put';
 const adminAPI = {
   getAllUsers: get.getAllUsers,
+  getAllShops: get.getAllShops,
 
   activateUser: put.activateUser,
   deactivateUser: put.deactivateUser,
+  activateShop: put.activateShop,
+  deactivateShop: put.deactivateShop,
 };
 export default adminAPI;
