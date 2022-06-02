23362a18fe7a7b425cef1258eab8208a409ec5eb
KoahF <khoakg2013@gmail.com>
2022-06-02 16:53:48 +0700

    Add AdminShopManagementPage

---
+++ src/pages/AdminShopManagementPage/AdminShopManagementPage.tsx
@@ -0,0 +1,116 @@
+import React, { FC, useEffect, useState } from 'react';
+import { FormattedMessage, injectIntl, IntlShape } from 'react-intl';
+import { toast } from 'react-toastify';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+
+import { BsSearch } from 'react-icons/bs';
+
+import { Paginator } from '../../components';
+import style from './AdminShopManagementPage.module.css';
+import adminAPI from '../../api/admin';
+import { shop } from '../../app/util/interfaces';
+import ShopsTable from './ShopsTable';
+
+const ITEMS_PER_PAGE = 5;
+
+interface Props {
+  intl: IntlShape;
+}
+
+const AdminShopManagementPage: FC<Props> = props => {
+  const { intl } = props;
+  const searchBarPlaceholder = intl.formatMessage({
+    id: 'CommonNavbar.searchBarPlaceholder',
+    defaultMessage: 'Search',
+  });
+
+  const [shops, setShops] = useState<shop[] | null>(null);
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+
+  useEffect(() => {
+    const getAllShops = async () => {
+      try {
+        const res: any = await adminAPI.getAllShops(1, ITEMS_PER_PAGE);
+        console.log(res);
+        const { shops, totalPages, page } = res;
+        setShops(shops);
+        setCurrentPage(page);
+        setTotalPages(totalPages);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllShops();
+  }, []);
+  const goToPage = async (pageNumber: number) => {
+    setShops(null);
+    try {
+      const res: any = await adminAPI.getAllShops(pageNumber, ITEMS_PER_PAGE);
+      const { shops, totalPages } = res;
+      setShops(shops);
+      setCurrentPage(pageNumber);
+      setTotalPages(totalPages);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleDeactivateShop = async (shopId: string) => {
+    const prevShops = shops!;
+
+    const newShops = shops!.map(shop =>
+      shopId === shop._id ? { ...shop, shopStatus: 0 } : { ...shop },
+    );
+    try {
+      setShops(newShops);
+      await adminAPI.deactivateShop(shopId);
+      toast.success('Action completed successfully');
+    } catch (error) {
+      setShops(prevShops);
+      toast.error('An unknown error occurred');
+    }
+  };
+  const handleActivateShop = async (shopId: string) => {
+    const prevShops = shops!;
+
+    const newShops = shops!.map(shop =>
+      shopId === shop._id ? { ...shop, shopStatus: 1 } : { ...shop },
+    );
+    try {
+      setShops(newShops);
+      await adminAPI.activateShop(shopId);
+      toast.success('Action completed successfully');
+    } catch (error) {
+      setShops(prevShops);
+      toast.error('An unknown error occurred');
+    }
+  };
+  return (
+    <Container className={`bg-white w-75 py-4 px-4 mt-3 rounded ${style.container}`}>
+      <Form className={style.searchBarWrapper}>
+        <div className={style.searchBar}>
+          <BsSearch className={style.searchIcon} />
+          <Form.Control
+            type="text"
+            placeholder={searchBarPlaceholder}
+            // onChange={e => handleChange(e as any)}
+          />
+        </div>
+      </Form>
+      <ShopsTable
+        shops={shops}
+        onDeactivateShop={handleDeactivateShop}
+        onActivateShop={handleActivateShop}
+      />
+      <Paginator
+        totalNumberOfPages={totalPages}
+        currentPage={currentPage}
+        handleClickGoToPage={(number: number) => goToPage(number)}
+      />
+    </Container>
+  );
+};
+
+export default injectIntl(AdminShopManagementPage);
---
+++ src/pages/AdminShopManagementPage/ShopsTable.tsx
@@ -0,0 +1,89 @@
+import { FC } from 'react';
+import { FormattedMessage, IntlShape, injectIntl } from 'react-intl';
+
+import { BsChevronDown } from 'react-icons/bs';
+
+import { Column, shop } from '../../app/util/interfaces';
+import { Table } from '../../components';
+import style from '../AdminUserManagementPage/AdminUserManagementPage.module.css';
+
+interface Props {
+  shops: shop[] | null;
+  onActivateShop: Function;
+  onDeactivateShop: Function;
+  intl: IntlShape;
+}
+
+const ShopsTable: FC<Props> = props => {
+  const { shops, onActivateShop, onDeactivateShop, intl } = props;
+  const storeNameTableHeaderLabel = intl.formatMessage({
+    id: 'AdminShopManagementPage.storeNameTableHeaderLabel',
+  });
+  const storeEmailTableHeaderLabel = intl.formatMessage({
+    id: 'AdminShopManagementPage.storeEmailTableHeaderLabel',
+  });
+  const ownerIdTableHeaderLabel = intl.formatMessage({
+    id: 'AdminShopManagementPage.ownerIdTableHeaderLabel',
+  });
+  const actionDropdownPlaceholder = (
+    <FormattedMessage id="AdminUserManagementPage.actionDropdownPlaceholder" />
+  );
+  const activateDropdownLabel = (
+    <FormattedMessage id="AdminUserManagementPage.activateDropdownLabel" />
+  );
+  const deactivateDropdownLabel = (
+    <FormattedMessage id="AdminUserManagementPage.deactivateDropdownLabel" />
+  );
+  const actionTableHeaderLabel = intl.formatMessage({
+    id: 'AdminUserManagementPage.actionTableHeaderLabel',
+  });
+  const columns: Column[] = [
+    { key: 'check', content: () => null, width: 5 },
+    { path: 'shopName', label: storeNameTableHeaderLabel, width: 25, bold: true },
+    { path: 'shopEmail', label: storeEmailTableHeaderLabel, width: 25 },
+    { path: 'userId', label: ownerIdTableHeaderLabel, width: 25, bold: true },
+    {
+      key: 'action',
+      label: actionTableHeaderLabel,
+      width: 20,
+      content: (shop: shop) => (
+        <div className={`dropdown`}>
+          <div
+            id="actionsMenuDropdown"
+            role="button"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+            className={`${style.actionBtn}`}
+          >
+            <span className={`me-2`}>{actionDropdownPlaceholder}</span>
+            <BsChevronDown style={{ color: '#5c5c5c' }} />
+          </div>
+          <ul
+            className="dropdown-menu position-absolute mt-1"
+            aria-labelledby="actionsMenuDropdown"
+          >
+            <li>
+              {shop.shopStatus > 0 ? (
+                <span
+                  onClick={() => onDeactivateShop(shop._id)}
+                  className="dropdown-item"
+                >
+                  {deactivateDropdownLabel}
+                </span>
+              ) : (
+                <span onClick={() => onActivateShop(shop._id)} className="dropdown-item">
+                  {activateDropdownLabel}
+                </span>
+              )}
+            </li>
+          </ul>
+        </div>
+      ),
+    },
+  ];
+  return (
+    <Table columns={columns} data={shops} className={`w-100 mt-3`} rowKeyPath={'_id'} />
+  );
+};
+
+export default injectIntl(ShopsTable);
