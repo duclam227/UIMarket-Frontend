509d9819985ad032bd0ba793b3afa9eb84f017ed
Duc-Lam-jh <duclam227@gmail.com>
2022-06-17 20:36:08 +0700

    add lazy loading

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { ReactElement, useEffect, useRef, useState } from 'react';
 import { toast } from 'react-toastify';
 import { useSelector } from 'react-redux';
 import { FormattedMessage, injectIntl, IntlShape } from 'react-intl';
@@ -18,6 +18,10 @@ import SectionTrendingCategories from './SectionTrendingCategories';
 import SectionStartEarning from './SectionStartEarning';
 
 import style from './LandingPage.module.css';
+import classNames from 'classnames';
+import { useIntersectionObserver } from '../../app/util/customHooks';
+
+const VIEW_THRESHOLD = 0.7;
 
 interface IProps {
   intl: IntlShape;
@@ -29,6 +33,13 @@ const LandingPage: React.FC<IProps> = props => {
   const currentUser = useSelector((state: State) => state.auth.user);
   const [trendingProducts, setTrendingProducts] = useState<Array<product> | null>(null);
   const [themeProducts, setThemeProducts] = useState<Array<product> | null>(null);
+
+  const sectionIntroRef = useRef<HTMLDivElement | null>(null);
+  const sectionTrendingProductsRef = useRef(null);
+  const sectionTrendingCategoriesRef = useRef(null);
+  const sectionFeaturedProductsRef = useRef(null);
+  const sectionExploreForumRef = useRef(null);
+  const sectionStartEarningRef = useRef(null);
 
   const trendingProductsTitle = intl.formatMessage({
     id: 'LandingPage.SectionTrendingProductsTitle',
@@ -72,24 +83,52 @@ const LandingPage: React.FC<IProps> = props => {
       });
   }, []);
 
+  const checkAnimation = (entryArray: Array<any>) => {
+    //entry array stores the one item that is registered to an observer
+    const entry = entryArray[0];
+    if (entry.isIntersecting) {
+      const entryElement = entry.target;
+      entryElement.className = classNames(style.show);
+    }
+  }
+
+  useIntersectionObserver(sectionIntroRef, checkAnimation, VIEW_THRESHOLD);
+  useIntersectionObserver(sectionTrendingProductsRef, checkAnimation, VIEW_THRESHOLD);
+  useIntersectionObserver(sectionTrendingCategoriesRef, checkAnimation, VIEW_THRESHOLD);
+  useIntersectionObserver(sectionFeaturedProductsRef, checkAnimation, VIEW_THRESHOLD);
+  useIntersectionObserver(sectionExploreForumRef, checkAnimation, VIEW_THRESHOLD);
+  useIntersectionObserver(sectionStartEarningRef, checkAnimation, VIEW_THRESHOLD);
+
   return (
     <PageWithNavbar>
-      <SectionHero currentUser={currentUser} />
-      <SectionFeaturedProducts
-        products={trendingProducts}
-        title={trendingProductsTitle}
-        subtitle={trendingProductsSubtitle}
-        link="/products"
-      />
-      <SectionExploreForum />
-      <SectionTrendingCategories />
-      <SectionFeaturedProducts
-        products={themeProducts}
-        title={themeProductsTitle}
-        subtitle={themeProductsSubtitle}
-        link="/products/category/627bc2f599bc9a68d1105497"
-      />
-      <SectionStartEarning />
+      <div ref={sectionIntroRef} className={style.hide}>
+        <SectionHero currentUser={currentUser} />
+      </div>
+      <div ref={sectionTrendingProductsRef} className={style.hide}>
+        <SectionFeaturedProducts
+          products={trendingProducts}
+          title={trendingProductsTitle}
+          subtitle={trendingProductsSubtitle}
+          link="/products"
+        />
+      </div>
+      <div ref={sectionTrendingCategoriesRef} className={style.hide}>
+        <SectionTrendingCategories />
+      </div>
+      <div ref={sectionFeaturedProductsRef} className={style.hide}>
+        <SectionFeaturedProducts
+          products={themeProducts}
+          title={themeProductsTitle}
+          subtitle={themeProductsSubtitle}
+          link="/products/category/627bc2f599bc9a68d1105497"
+        />
+      </div>
+      <div ref={sectionExploreForumRef} className={style.hide}>
+        <SectionExploreForum />
+      </div>
+      <div ref={sectionStartEarningRef} className={style.hide}>
+        <SectionStartEarning />
+      </div>
     </PageWithNavbar>
   );
 };
