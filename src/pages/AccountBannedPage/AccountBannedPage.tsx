01d74f6919184cfee51ba52e943bb298f3df28b8
Kim Nguyễn Duy Thiên <ngduytkim@gmail.com>
2022-06-25 01:36:21 +0700

    account banned page layout

--- src/pages/AccountBannedPage/AccountBannedPage.tsx
+++ src/pages/AccountBannedPage/AccountBannedPage.tsx
@@ -1,54 +1,45 @@
 import { Link } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-
 import { BsChevronLeft } from 'react-icons/bs';
 
-import FlatCross from '../../app/assets/FlatCross.svg';
-import { AuthenticationLayout } from '../../components';
+import errorImg from '../../app/assets/error-general.png';
+import { AuthenticationLayout, EmptyState } from '../../components';
 import { FormattedMessage } from 'react-intl';
+import styles from './AccountBannedPage.module.css';
+import PageWithNavbar from '../../components/common/PageWithNavbar/PageWithNavbar';
 
 const AccountBannedPage = () => {
   const pageTitle = <FormattedMessage id="AccountBannedPage.pageTitle" />;
-  const accountBannedMessage = (
-    <FormattedMessage id="AccountBannedPage.accountBannedMessage" />
-  );
   const contactUsMessage = <FormattedMessage id="AccountBannedPage.contactUsMessage" />;
   const backSignInNavLabel = (
     <FormattedMessage id="RecoverPasswordForm.backSignInNavLabel" />
   );
   return (
-    <AuthenticationLayout>
-      <Container className={`pt-5`}>
-        <Col sm={{ span: 7, offset: 3 }}>
-          {/* Navigate to Sign In */}
-          <Row className="mt-5">
-            <Link
-              to="/login"
-              className="d-flex align-items-center"
-              style={{ width: 'fit-content' }}
-            >
+    <PageWithNavbar>
+      <Container>
+        <div className={styles.formContainer}>
+          <div className={'mt-4 ' + styles.title}>
+            {/* Navigate to Sign In */}
+            <Link to="/login" className="d-flex align-items-center">
               <div className="d-flex align-items-center justify-content center me-2">
                 <BsChevronLeft />
               </div>
               <span>{backSignInNavLabel}</span>
             </Link>
-          </Row>
-
-          {/* Title */}
-          <Row className="mt-4">
-            <h2>{pageTitle}</h2>
-          </Row>
-          <Row className={'flex-column justify-content-center align-items-center'}>
-            <img src={FlatCross} alt="Check email" height={200}></img>
-            <span className="text-center mt-3">{accountBannedMessage}</span>
-            <span className="text-center">{contactUsMessage}</span>
-          </Row>
-        </Col>
+            {/* Title */}
+            <h2 className="mt-4 text-center">{pageTitle}</h2>
+          </div>
+          <EmptyState
+            img={errorImg}
+            messageId="AccountBannedPage.accountBannedMessage"
+            btn={false}
+          >
+            <span className="text-center text-black">{contactUsMessage}</span>
+          </EmptyState>
+        </div>
       </Container>
-    </AuthenticationLayout>
+    </PageWithNavbar>
   );
 };
 
