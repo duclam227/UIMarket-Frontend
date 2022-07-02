c3392ca7c57f0c12b23d34a0691586e5035ce32c
KoahF <khoakg2013@gmail.com>
2022-07-02 15:07:12 +0700

    Add pagination TagListPage

--- src/pages/TagListPage/TagListPage.tsx
+++ src/pages/TagListPage/TagListPage.tsx
@@ -13,14 +13,16 @@ import { PageWithNavbar, Paginator } from '../../components';
 
 import style from './TagListPage.module.css';
 
-const ITEMS_PER_PAGE = 15;
+const ITEMS_PER_PAGE = 7;
 
 interface IProps {
-  intl: IntlShape
+  intl: IntlShape;
 }
 
-const TagListPage: FC<IProps> = (props) => {
+const TagListPage: FC<IProps> = props => {
   const { intl } = props;
+  const [totalPages, setTotalPages] = useState<number>(1);
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [tags, setTags] = useState<Array<any> | null>(null);
@@ -30,47 +32,75 @@ const TagListPage: FC<IProps> = (props) => {
       <div key={tag._id} className={style.tagItem}>
         <div className={style.tagName}>
           <Link to={`/questions/tag/${tag.tagName}`}>
-            <Badge pill>
-              {tag.tagName}
-            </Badge>
+            <Badge pill>{tag.tagName}</Badge>
           </Link>
         </div>
         <div className={style.tagNumber}>
-          <FormattedMessage id='TagListPage.numberOfQuestions' values={{ count: tag.totalQuestion }} />
+          <FormattedMessage
+            id="TagListPage.numberOfQuestions"
+            values={{ count: tag.totalQuestion }}
+          />
         </div>
       </div>
-    )
-  }
+    );
+  };
 
   useEffect(() => {
-    setIsLoading(true)
-    questionAPI.getTagListByPage(1, ITEMS_PER_PAGE)
+    setIsLoading(true);
+    questionAPI
+      .getTagListByPage(1, ITEMS_PER_PAGE)
       .then((res: any) => {
-        setTags([...res]);
+        const { tags, totalPages, page } = res;
+        setCurrentPage(page);
+        setTotalPages(totalPages);
+        setTags([...tags]);
       })
       .catch(error => {
+        console.log(error);
         const errorMsg = getErrorMessage(error);
-        const errorCode: any = errorCodes.question[errorMsg as keyof typeof errorCodes.question];
+        const errorCode: any =
+          errorCodes.question[errorMsg as keyof typeof errorCodes.question];
         toast.error(intl.formatMessage({ id: `Question.${errorCode}` }));
       })
-      .finally(() => setIsLoading(false))
-  }, [])
-
+      .finally(() => setIsLoading(false));
+  }, []);
+  const goToPage = async (pageNumber: number) => {
+    setIsLoading(true);
+    questionAPI
+      .getTagListByPage(pageNumber, ITEMS_PER_PAGE)
+      .then((res: any) => {
+        const { tags, totalPages, page } = res;
+        setCurrentPage(page);
+        setTotalPages(totalPages);
+        setTags([...tags]);
+      })
+      .catch(error => {
+        console.log(error);
+        const errorMsg = getErrorMessage(error);
+        const errorCode: any =
+          errorCodes.question[errorMsg as keyof typeof errorCodes.question];
+        toast.error(intl.formatMessage({ id: `Question.${errorCode}` }));
+      })
+      .finally(() => setIsLoading(false));
+  };
   return (
     <PageWithNavbar branch={navbarBranches.question}>
       <div className={style.wrapper}>
-        {isLoading
-          ? <Spinner animation='border' />
-          : <div className={style.container}>
-            {tags && tags.length > 0
-              ? tags.map((tag: any) => renderTag(tag))
-              : null
-            }
+        {isLoading ? (
+          <Spinner animation="border" />
+        ) : (
+          <div className={style.container}>
+            {tags && tags.length > 0 ? tags.map((tag: any) => renderTag(tag)) : null}
+            <Paginator
+              totalNumberOfPages={totalPages}
+              currentPage={currentPage}
+              handleClickGoToPage={(number: number) => goToPage(number)}
+            />
           </div>
-        }
+        )}
       </div>
     </PageWithNavbar>
-  )
+  );
 };
 
 export default injectIntl(TagListPage);
